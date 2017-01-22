CnCNet/xna-cncnet-client

commit 64292f34155ff9369303ca1448ec3a85f1ae6206

Fixed selected map switching to the default map for non-host players after a game

DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -190,6 +190,7 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
             if (IsHost)
             {
                 GenerateGameID();
+                DdGameMode_SelectedIndexChanged(null, EventArgs.Empty); // Refresh ranks
             }
 
             base.GameProcessExited();

DXMainClient/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.3.3")]
-[assembly: AssemblyFileVersion("2.0.3.3")]
+[assembly: AssemblyVersion("2.0.3.4")]
+[assembly: AssemblyFileVersion("2.0.3.4")]

DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -935,8 +935,6 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
 
             StatisticsManager.Instance.AddMatchAndSaveDatabase(true, matchStatistics);
 
-            DdGameMode_SelectedIndexChanged(null, EventArgs.Empty); // Refresh ranks
-
             ClearReadyStatuses();
 
             CopyPlayerDataToUI();

DXMainClient/DXGUI/Multiplayer/GameLobby/SkirmishLobby.cs
@@ -155,6 +155,8 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
 
         protected override void GameProcessExited()
         {
+            DdGameMode_SelectedIndexChanged(null, EventArgs.Empty); // Refresh ranks
+
             base.GameProcessExited();
 
             RandomSeed = new Random().Next();

commit 6c6b7f6e32fa6a7027a9369cc2a0e52654f2f01c

Fixed map ranks not updating correctly after a game on CnCNet
The map that was played didn't refresh its rank, because the map list was refreshed before the just-played match was added to the statistics
This bug was introduced by commit 64292f34155ff9369303ca1448ec3a85f1ae6206

DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -187,13 +187,13 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
         {
             gameSaved = false;
 
+            base.GameProcessExited();
+
             if (IsHost)
             {
                 GenerateGameID();
                 DdGameMode_SelectedIndexChanged(null, EventArgs.Empty); // Refresh ranks
             }
-
-            base.GameProcessExited();
         }
 
         private void GenerateGameID()

DXMainClient/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.3.7")]
-[assembly: AssemblyFileVersion("2.0.3.7")]
+[assembly: AssemblyVersion("2.0.3.8")]
+[assembly: AssemblyFileVersion("2.0.3.8")]