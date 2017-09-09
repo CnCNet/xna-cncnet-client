da7f743b63994d93cab83519d04caae2f7e358bc
Rampastring <rampastring@outlook.com>
2017-09-09 14:48:38 +0300

    Added batch file for quickly forming the correct folder structure from compiled binaries
    Should look into automatizing the process later on.

---
+++ DXMainClient/compiler.bat
@@ -0,0 +1,67 @@
+echo Taking binaries compiled using Visual Studio and putting them into correct folders with the correct names for use in target projects. 
+cd bin
+rd /S CompiledResources
+mkdir CompiledResources
+cd CompiledResources
+mkdir Binaries
+cd Binaries
+mkdir OpenGL
+mkdir Windows
+mkdir XNA
+
+REM Set paths
+
+set cr="..\..\CompiledResources\"
+set commonBinaries="..\..\CompiledResources\Binaries\"
+set winBinaries="..\..\CompiledResources\Binaries\Windows\"
+set oglBinaries="..\..\CompiledResources\Binaries\OpenGL\"
+set xnaBinaries="..\..\CompiledResources\Binaries\XNA\"
+
+REM Windows
+
+cd ..\..\Windows\Release\
+copy DTAClient.exe %cr%clientdx.exe
+copy ClientCore.dll %winBinaries%ClientCore.dll
+copy ClientGUI.dll %winBinaries%ClientGUI.dll
+copy DTAConfig.dll %winBinaries%DTAConfig.dll
+copy Rampastring.XNAUI.dll %winBinaries%Rampastring.XNAUI.dll
+copy MonoGame.Framework.dll %winBinaries%MonoGame.Framework.dll
+copy SharpDX.Direct2D1.dll %winBinaries%SharpDX.Direct2D1.dll
+copy SharpDX.Direct3D9.dll %winBinaries%SharpDX.Direct3D9.dll
+copy SharpDX.Direct3D11.dll %winBinaries%SharpDX.Direct3D11.dll
+copy SharpDX.dll %winBinaries%SharpDX.dll
+copy SharpDX.DXGI.dll %winBinaries%SharpDX.DXGI.dll
+copy SharpDX.MediaFoundation.dll %winBinaries%SharpDX.MediaFoundation.dll
+copy SharpDX.RawInput.dll %winBinaries%SharpDX.RawInput.dll
+copy SharpDX.XAudio2.dll %winBinaries%SharpDX.XAudio2.dll
+copy SharpDX.XInput.dll %winBinaries%SharpDX.XInput.dll
+
+REM OpenGL
+
+cd ..\..\WindowsGL\Release\
+copy DTAClient.exe %cr%clientogl.exe
+copy ClientCore.dll %oglBinaries%ClientCore.dll
+copy ClientGUI.dll %oglBinaries%ClientGUI.dll
+copy DTAConfig.dll %oglBinaries%DTAConfig.dll
+copy MonoGame.Framework.dll %oglBinaries%MonoGame.Framework.dll
+copy NVorbis.dll %oglBinaries%NVorbis.dll
+copy OpenTK.dll %oglBinaries%OpenTK.dll
+copy Rampastring.XNAUI.dll %oglBinaries%Rampastring.XNAUI.dll
+
+REM XNA
+
+cd ..\..\XNAFramework\Release\
+copy DTAClient.exe %cr%clientxna.exe
+copy ClientCore.dll %xnaBinaries%ClientCore.dll
+copy ClientGUI.dll %xnaBinaries%ClientGUI.dll
+copy DTAConfig.dll %xnaBinaries%DTAConfig.dll
+copy Rampastring.XNAUI.dll %xnaBinaries%Rampastring.XNAUI.dll
+
+REM Common
+
+copy DTAUpdater.dll %commonBinaries%DTAUpdater.dll
+copy Ionic.Zip.dll %commonBinaries%Ionic.Zip.dll
+copy MapThumbnailExtractor.dll %commonBinaries%MapThumbnailExtractor.dll
+copy Rampastring.Tools.dll %commonBinaries%Rampastring.Tools.dll
+
+pause Copying complete.
