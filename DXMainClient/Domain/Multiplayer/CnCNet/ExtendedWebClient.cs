bb3dbc83c77f64a4864d497ec68ee10da7e246f2
Rampastring <rampastring@outlook.com>
2018-04-02 14:18:16 +0300

    When starting a CnCNet online game, the client now times out when the tunnel fails to respond within 10 seconds Previously the client waited for the timeout of the default WebClient implementation, which is 100 seconds and way too long
    Also improved CnCNetTunnel and TunnelHandler code

---
+++ DXMainClient/Domain/Multiplayer/CnCNet/ExtendedWebClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace DTAClient.Domain.Multiplayer.CnCNet
+{
+    /// <summary>
+    /// A web client that supports customizing the timeout of the request.
+    /// </summary>
+    class ExtendedWebClient : WebClient
+    {
+        public ExtendedWebClient(int timeout)
+        {
+            this.timeout = timeout;
+        }
+
+        private int timeout;
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest webRequest = base.GetWebRequest(address);
+            webRequest.Timeout = timeout;
+            return webRequest;
+        }
+    }
+}
