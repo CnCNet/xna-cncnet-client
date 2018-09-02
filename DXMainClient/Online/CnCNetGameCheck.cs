2cf7dc956360231ecbe58c01508501520b3411da
Grant Bartlett <hello@grant-bartlett.com>
2018-09-02 17:10:14 +0100

    Dispose of process list in KillGameInstance

--- DXMainClient/Online/CnCNetGameCheck.cs
+++ DXMainClient/Online/CnCNetGameCheck.cs
@@ -70,6 +70,8 @@ namespace DTAClient.Online
                     {
                         process.Kill();
                     }
+
+                    process.Dispose();
                 }
             }
             catch
