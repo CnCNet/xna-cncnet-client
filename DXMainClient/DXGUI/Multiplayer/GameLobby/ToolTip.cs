46614df905248f5a981d460d310a03052cdec1fe
Rampastring <rampastring@outlook.com>
2018-11-25 17:22:22 +0200

    Make tooltips functional

--- DXMainClient/DXGUI/Multiplayer/GameLobby/ToolTip.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/ToolTip.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Rampastring.XNAUI;
+using Rampastring.XNAUI.XNAControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,69 +11,139 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
     /// <summary>
     /// A tool tip for a game option.
     /// </summary>
-    class ToolTip
+    class ToolTip : XNAControl
     {
         private const int FONT_INDEX = 0;
         private const int MARGIN = 6;
-        private const float ALPHA_RATE_PER_SECOND = 2.0f;
+        private const float ALPHA_RATE_PER_SECOND = 4.0f;
 
-        private string text;
-        public string Text
+        /// <summary>
+        /// The time in seconds that the cursor must stay still on top of the 
+        /// master control for the tooltip to be displayed.
+        /// </summary>
+        private const float SECONDS_BEFORE_DISPLAYING = 0.67f;
+
+        /// <summary>
+        /// Creates a new tool tip and attaches it to the given control.
+        /// </summary>
+        /// <param name="windowManager">The window manager.</param>
+        /// <param name="masterControl">The control to attach the tool tip to.</param>
+        public ToolTip(WindowManager windowManager, XNAControl masterControl) : base(windowManager)
         {
-            get { return text; }
+            if (masterControl == null)
+                throw new ArgumentNullException("masterControl");
+
+            this.masterControl = masterControl;
+            masterControl.MouseEnter += MasterControl_MouseEnter;
+            masterControl.MouseLeave += MasterControl_MouseLeave;
+            masterControl.MouseMove += MasterControl_MouseMove;
+            masterControl.EnabledChanged += MasterControl_EnabledChanged;
+            InputEnabled = false;
+            DrawOrder = int.MinValue;
+            // TODO: adding tool tips as root-level controls might be CPU-intensive.
+            // instead we could find out the root-level parent and only have the tooltip
+            // in the window manager's list when the root-level parent is visible.
+            WindowManager.AddControl(this);
+            Visible = false;
+        }
+
+        private void MasterControl_EnabledChanged(object sender, EventArgs e)
+        {
+            Enabled = masterControl.Enabled;
+        }
+
+        public override string Text
+        {
+            get { return base.Text; }
             set
             {
-                text = value;
-                Vector2 textSize = Renderer.GetTextDimensions(text, FONT_INDEX);
-                Size = new Point((int)textSize.X + MARGIN * 2, (int)textSize.Y + MARGIN * 2);
+                base.Text = value;
+                Vector2 textSize = Renderer.GetTextDimensions(base.Text, FONT_INDEX);
+                Width = (int)textSize.X + MARGIN * 2;
+                Height = (int)textSize.Y + MARGIN * 2;
             }
         }
 
-        public Point Location { get; set; }
-        public Point Size { get; private set; }
-        public float Alpha { get; private set; }
+        public override float Alpha { get; set; }
         public bool IsMasterControlOnCursor { get; set; }
-        public bool IsVisible { get; private set; }
 
-        public void Update(GameTime gameTime)
+        private XNAControl masterControl;
+
+        private TimeSpan cursorTime = TimeSpan.Zero;
+        
+
+        private void MasterControl_MouseEnter(object sender, EventArgs e)
+        {
+            DisplayAtLocation(WindowManager.Cursor.Location + new Point(0, 5));
+            IsMasterControlOnCursor = true;
+        }
+
+        private void MasterControl_MouseLeave(object sender, EventArgs e)
+        {
+            IsMasterControlOnCursor = false;
+            cursorTime = TimeSpan.Zero;
+        }
+
+        private void MasterControl_MouseMove(object sender, EventArgs e)
+        {
+            cursorTime = TimeSpan.Zero;
+        }
+
+        /// <summary>
+        /// Sets the tool tip's location, checking that it doesn't exceed the window's bounds.
+        /// </summary>
+        /// <param name="location">The location.</param>
+        public void DisplayAtLocation(Point location)
+        {
+            X = location.X + Width > WindowManager.RenderResolutionX ?
+                WindowManager.RenderResolutionX - Width : location.X;
+            Y = location.Y;
+        }
+
+        public override void Update(GameTime gameTime)
         {
             if (IsMasterControlOnCursor)
             {
-                Alpha += ALPHA_RATE_PER_SECOND * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                IsVisible = true;
-                if (Alpha > 1.0f)
-                    Alpha = 1.0f;
-            }
-            else
-            {
-                Alpha -= ALPHA_RATE_PER_SECOND * (float)gameTime.ElapsedGameTime.TotalSeconds;
-                if (Alpha < 0f)
+                cursorTime += gameTime.ElapsedGameTime;
+
+                if (cursorTime > TimeSpan.FromSeconds(SECONDS_BEFORE_DISPLAYING))
                 {
-                    Alpha = 0f;
-                    IsVisible = false;
+                    Alpha += ALPHA_RATE_PER_SECOND * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    Visible = true;
+                    if (Alpha > 1.0f)
+                        Alpha = 1.0f;
+                    return;
                 }
+            }
+
+            Alpha -= ALPHA_RATE_PER_SECOND * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (Alpha < 0f)
+            {
+                Alpha = 0f;
+                Visible = false;
             }
         }
 
-        public void Draw()
+        public override void Draw(GameTime gameTime)
         {
-            Renderer.FillRectangle(new Rectangle(Location.X, Location.Y, Size.X, Size.Y),
+            Renderer.FillRectangle(ClientRectangle,
                 ColorFromAlpha(UISettings.BackgroundColor));
-            Renderer.DrawRectangle(new Rectangle(Location.X, Location.Y, Size.X, Size.Y),
+            Renderer.DrawRectangle(ClientRectangle,
                 ColorFromAlpha(UISettings.AltColor));
             Renderer.DrawString(Text, FONT_INDEX, 1.0f,
-                new Vector2(Location.X + MARGIN, Location.Y + MARGIN),
+                new Vector2(X + MARGIN, Y + MARGIN),
                 ColorFromAlpha(UISettings.AltColor));
         }
 
         private Color ColorFromAlpha(Color color)
         {
+            // This is necessary because XNA lacks the color constructor that
+            // takes a color and a float value for alpha.
 #if XNA
             return new Color(color.R, color.G, color.B, (int)(Alpha * 255.0f));
 #else
             return new Color(color, Alpha);
 #endif
-
         }
     }
 }
