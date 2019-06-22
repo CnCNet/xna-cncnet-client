553684250907a1c80382a1b7674c86dcbc280972
Rampastring <rampastring@outlook.com>
2019-06-22 15:02:49 +0300

    Start work on version 3 tunnel support

---
+++ DXMainClient/Domain/Multiplayer/CnCNet/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace DTAClient.Domain.Multiplayer.CnCNet
+{
+    class Constants
+    {
+        internal const int TUNNEL_CONNECTION_TIMEOUT = 10000; // In milliseconds
+        internal const int TUNNEL_RECEIVE_TIMEOUT = 30000;
+
+        internal const int TUNNEL_VERSION_2 = 2;
+        internal const int TUNNEL_VERSION_3 = 3;
+    }
+}
