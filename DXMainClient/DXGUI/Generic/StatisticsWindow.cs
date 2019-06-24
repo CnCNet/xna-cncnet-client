82eecd148f37bd81d05eaaa77e4b14bd94937708
Rampastring <rampastring@outlook.com>
2019-06-24 23:29:55 +0300

    Improve format of "total time played" and "average game length" in the statistics window

--- DXMainClient/DXGUI/Generic/StatisticsWindow.cs
+++ DXMainClient/DXGUI/Generic/StatisticsWindow.cs
@@ -922,7 +922,7 @@ namespace DTAClient.DXGUI.Generic
 
             if (gamesStarted > 0)
             {
-                lblAverageGameLengthValue.Text = TimeSpan.FromSeconds((int)timePlayed.TotalSeconds / gamesStarted).ToString();
+                lblAverageGameLengthValue.Text = TimeSpanToString(TimeSpan.FromSeconds((int)timePlayed.TotalSeconds / gamesStarted));
             }
             else
                 lblAverageGameLengthValue.Text = "-";
@@ -951,7 +951,7 @@ namespace DTAClient.DXGUI.Generic
             else
                 lblKillLossRatioValue.Text = "-";
 
-            lblTotalTimePlayedValue.Text = timePlayed.ToString();
+            lblTotalTimePlayedValue.Text = TimeSpanToString(timePlayed);
             lblTotalKillsValue.Text = totalKills.ToString();
             lblTotalLossesValue.Text = totalLosses.ToString();
             lblTotalScoreValue.Text = totalScore.ToString();
@@ -963,6 +963,13 @@ namespace DTAClient.DXGUI.Generic
                 lblAverageAILevelValue.Text = "Medium".L10N("Client:Main:MediumAI");
             else
                 lblAverageAILevelValue.Text = "Hard".L10N("Client:Main:HardAI");
+        }
+
+        private string TimeSpanToString(TimeSpan timeSpan)
+        {
+            return timeSpan.Days > 0 ?
+                    $"{timeSpan.Days} d {timeSpan.Hours} h {timeSpan.Minutes} m {timeSpan.Seconds} s" :
+                    $"{timeSpan.Hours} h {timeSpan.Minutes} m {timeSpan.Seconds} s";
         }
 
         private PlayerStatistics FindLocalPlayer(MatchStatistics ms)
