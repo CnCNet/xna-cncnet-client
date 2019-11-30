b19c449e12890f2ff03e4fe5b41c4dfe4aecdb11
Starkku <starkku.ppmod@gmail.com>
2019-11-30 11:18:34 +0200

    Revert "Added UseStarDisplay property to GameLaunchButton that can be used to disable the star display through UI INI system."
    This reverts commit 05618372f0568b97332438a7d1ae21e464f22f62.

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLaunchButton.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLaunchButton.cs
@@ -1,9 +1,13 @@
 ﻿using ClientGUI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Rampastring.Tools;
 using Rampastring.XNAUI;
 using Rampastring.XNAUI.XNAControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DTAClient.DXGUI.Multiplayer.GameLobby
 {
@@ -16,7 +20,6 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
         }
 
         private readonly StarDisplay starDisplay;
-        private bool UseStarDisplay = true;
 
         public override void Initialize()
         {
@@ -33,34 +36,14 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
 
         private void UpdateStarPosition()
         {
-            if (UseStarDisplay)
-            {
-                starDisplay.Visible = true;
-                starDisplay.Y = (Height - starDisplay.Height) / 2;
-                starDisplay.X = (Width / 2) + (int)(Renderer.GetTextDimensions(Text, FontIndex).X / 2) + 3;
-            }
-            else
-            {
-                starDisplay.Visible = false;
-            }
+            starDisplay.Y = (Height - starDisplay.Height) / 2;
+            starDisplay.X = (Width / 2) + (int)(Renderer.GetTextDimensions(Text, FontIndex).X / 2) + 3;
         }
 
         public void SetRank(int rank)
         {
             starDisplay.Rank = rank;
             UpdateStarPosition();
-        }
-
-        protected override void ParseAttributeFromINI(IniFile iniFile, string key, string value)
-        {
-            switch (key)
-            {
-                case "UseStarDisplay":
-                    UseStarDisplay = Conversions.BooleanFromString(value, true);
-                    return;
-            }
-
-            base.ParseAttributeFromINI(iniFile, key, value);
         }
     }
 
