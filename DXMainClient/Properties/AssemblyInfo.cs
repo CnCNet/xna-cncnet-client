fb2fe46fb465f4f97bf298e32f793f88ba54fc0a
Rampastring <rampastring@outlook.com>
2020-01-22 18:42:32 +0200

    Fix crash when map preview extractor fails

--- DXMainClient/Properties/AssemblyInfo.cs
+++ DXMainClient/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rampastring")]
 [assembly: AssemblyProduct("Dawn of the Tiberium Age")]
-[assembly: AssemblyCopyright("Copyright © Rampastring 2011-2019")]
+[assembly: AssemblyCopyright("Copyright © Rampastring 2011-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.1.1")]
-[assembly: AssemblyFileVersion("2.5.1.1")]
+[assembly: AssemblyVersion("2.5.1.2")]
+[assembly: AssemblyFileVersion("2.5.1.2")]
