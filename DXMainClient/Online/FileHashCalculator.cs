e4b23df2d5e9f7f5f8826d1d2ae01a5ddad976d8
Starkku <starkku.ppmod@gmail.com>
2020-03-10 01:43:18 +0200

    Improvements & refactoring to FileHashCalculator & FHCConfig.ini parsing.
    - Now reads entries using proper INI format (key=value) under [FilenameList]. Old format also still supported for backwards compatibility.
    - FHCConfig.ini is now included amongst the files in FileHashes, instead of being appended to the filenames-to-check list.
    - GlobalCode.ini is no longer added to list of filenames parsed from FHCConfig.ini by default on YRRelease build.

--- DXMainClient/Online/FileHashCalculator.cs
+++ DXMainClient/Online/FileHashCalculator.cs
@@ -11,8 +11,8 @@ namespace DTAClient.Online
 {
     public class FileHashCalculator
     {
-        FileHashes fh;
-        const string CONFIGNAME = "FHCConfig.ini";
+        private FileHashes fh;
+        private const string CONFIGNAME = "FHCConfig.ini";
 
         string[] fileNamesToCheck = new string[]
         {
@@ -61,13 +61,12 @@ namespace DTAClient.Online
             "INI\\AIE.ini",
             "INI\\AIFS.ini",
 #endif
-            ProgramConstants.BASE_RESOURCE_PATH + CONFIGNAME,
         };
 
 
         public FileHashCalculator()
         {
-            GetFileListFromConfig();
+            ParseConfigFile();
         }
 
         public void CalculateHashes(List<GameMode> gameModes)
@@ -81,6 +80,7 @@ namespace DTAClient.Online
                 MainExeHash = Utilities.CalculateSHA1ForFile(ProgramConstants.GamePath + ClientConfiguration.Instance.GetGameExecutableName()),
                 LauncherExeHash = Utilities.CalculateSHA1ForFile(ProgramConstants.GamePath + ClientConfiguration.Instance.GameLauncherExecutableName),
                 MPMapsHash = Utilities.CalculateSHA1ForFile(ProgramConstants.GamePath + ClientConfiguration.Instance.MPMapsIniPath),
+                FHCConfigHash = Utilities.CalculateSHA1ForFile(ProgramConstants.BASE_RESOURCE_PATH + CONFIGNAME),
                 INIHashes = string.Empty
             };
 
@@ -110,10 +110,9 @@ namespace DTAClient.Online
 
                     foreach (string fileName in files)
                     {
-                        fh.INIHashes = fh.INIHashes + Utilities.CalculateSHA1ForFile(fileName);
+                        fh.INIHashes += Utilities.CalculateSHA1ForFile(fileName);
                         Logger.Log("Hash for " + fileName.Replace(ProgramConstants.GamePath, "") + 
                             ": " + Utilities.CalculateSHA1ForFile(fileName));
-
                     }
                 }
             }
@@ -134,28 +133,35 @@ namespace DTAClient.Online
         public string GetCompleteHash()
         {
             string str = fh.GameOptionsHash;
-            str = str + fh.ClientDXHash;
-            str = str + fh.ClientXNAHash;
-            str = str + fh.ClientOGLHash;
-            str = str + fh.MainExeHash;
-            str = str + fh.LauncherExeHash;
-            str = str + fh.INIHashes;
-            str = str + fh.MPMapsHash;
+            str += fh.ClientDXHash;
+            str += fh.ClientXNAHash;
+            str += fh.ClientOGLHash;
+            str += fh.MainExeHash;
+            str += fh.LauncherExeHash;
+            str += fh.INIHashes;
+            str += fh.MPMapsHash;
+            str += fh.FHCConfigHash;
 
             Logger.Log("Complete hash: " + Utilities.CalculateSHA1ForString(str));
 
             return Utilities.CalculateSHA1ForString(str);
         }
 
-        private void GetFileListFromConfig()
+        private void ParseConfigFile()
         {
-            IniFile filenamesconfig = new IniFile(ProgramConstants.GetBaseResourcePath() + CONFIGNAME);
-            List<string> filenames = filenamesconfig.GetSectionKeys("FilenameList");
-            if (filenames == null || filenames.Count < 1) return;
-#if YR
-            filenames.Add("INI\\Map Code\\GlobalCode.ini");
-#endif
-            filenames.Add(ProgramConstants.BASE_RESOURCE_PATH + CONFIGNAME);
+            IniFile config = new IniFile(ProgramConstants.GetBaseResourcePath() + CONFIGNAME);
+
+            List<string> keys = config.GetSectionKeys("FilenameList");
+            if (keys == null || keys.Count < 1)
+                return;
+
+            List<string> filenames = new List<string>();
+            foreach (string key in keys)
+            {
+                string value = config.GetStringValue("FilenameList", key, string.Empty);
+                filenames.Add(value == string.Empty ? key : value);
+            }
+
             fileNamesToCheck = filenames.ToArray();
         }
     }
@@ -170,6 +176,7 @@ namespace DTAClient.Online
         public string MPMapsHash { get; set; }
         public string MainExeHash { get; set; }
         public string LauncherExeHash { get; set; }
+        public string FHCConfigHash { get; set; }
 
         public override string ToString()
         {
@@ -180,7 +187,8 @@ namespace DTAClient.Online
                 "INI Hashes: " + INIHashes + Environment.NewLine +
                 "MPMaps Hash: " + MPMapsHash + Environment.NewLine +
                 "MainExe Hash: " + MainExeHash + Environment.NewLine +
-                "LauncherExe Hash: " + LauncherExeHash;
+                "LauncherExe Hash: " + LauncherExeHash + Environment.NewLine +
+                "FHCConfig Hash: " + FHCConfigHash;
         }
     }
 }
