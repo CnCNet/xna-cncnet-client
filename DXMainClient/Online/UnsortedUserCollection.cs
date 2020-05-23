2fde3c68e99849b8f57a621ba04fa55cbe5e3437
Rampastring <rampastring@outlook.com>
2020-05-23 15:23:26 +0300

    Use better-scaling data structures instead of lists for channel users
    - Non-chat channels (such as game broadcast channels) use a dictionary, because there's no need for them to be sorted as they're never displayed in the UI
    - Chat channels use a dictionary mixed with a linked list to allow O(1) lookups and removals, additions are O(n) due to the need for keeping the list sorted
    - For chat channels, the data structure itself makes sure that it's properly ordered, and the order is retained without re-creating the data structure as was done before (OrderBy sorted a copy of the list)

---
+++ DXMainClient/Online/UnsortedUserCollection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTAClient.Online
+{
+    /// <summary>
+    /// A custom collection that aims to provide quick insertion,
+    /// removal and lookup operations by using a dictionary. Does not
+    /// keep the list sorted.
+    /// </summary>
+    public class UnsortedUserCollection<T> : IUserCollection<T>
+    {
+        private Dictionary<string, T> dictionary = new Dictionary<string, T>();
+
+        public int Count => dictionary.Count;
+
+        public void Add(string username, T item)
+        {
+            dictionary.Add(username, item);
+        }
+
+        public void Clear()
+        {
+            dictionary.Clear();
+        }
+
+        public void DoForAllUsers(Action<T> action)
+        {
+            var values = dictionary.Values;
+            
+            foreach (T value in values)
+            {
+                action(value);
+            }
+        }
+
+        public T Find(string username)
+        {
+            if (dictionary.TryGetValue(username, out T value))
+                return value;
+
+            return default(T);
+        }
+
+        public LinkedListNode<T> GetFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reinsert(string username)
+        {
+            // We have no need to do this
+        }
+
+        public void Remove(string username)
+        {
+            dictionary.Remove(username);
+        }
+    }
+}
