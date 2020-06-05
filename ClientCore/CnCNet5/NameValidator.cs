3564265adeb92174cf1306ad96b20be25bc5aa3c
Starkku <starkku.ppmod@gmail.com>
2020-06-05 18:44:08 +0300

    Improve player nickname validation on startup & on saving settings.

--- ClientCore/CnCNet5/NameValidator.cs
+++ ClientCore/CnCNet5/NameValidator.cs
@@ -5,6 +5,8 @@ namespace ClientCore.CnCNet5
 {
     public static class NameValidator
     {
+        private static readonly char[] ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_[]|\\{}^`".ToCharArray();
+
         /// <summary>
         /// Checks if the player's nickname is valid for CnCNet.
         /// </summary>
@@ -20,20 +22,18 @@ namespace ClientCore.CnCNet5
             if (profanityFilter.IsOffensive(name))
                 return "Please enter a name that is less offensive.";
 
-            int number = -1;
-            if (int.TryParse(name.Substring(0, 1), out number))
+            if (int.TryParse(name.Substring(0, 1), out _))
                 return "The first character in the player name cannot be a number.";
 
             if (name[0] == '-')
                 return "The first character in the player name cannot be a dash ( - ).";
 
             // Check that there are no invalid chars
-            char[] allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_[]|\\{}^`".ToCharArray();
             char[] nicknameChars = name.ToCharArray();
 
             foreach (char nickChar in nicknameChars)
             {
-                if (!allowedCharacters.Contains(nickChar))
+                if (!ALLOWED_CHARS.Contains(nickChar))
                 {
                     return "Your player name has invalid characters in it." + Environment.NewLine +
                     "Allowed characters are anything from A to Z and numbers.";
@@ -44,6 +44,22 @@ namespace ClientCore.CnCNet5
                 return "Your nickname is too long.";
 
             return null;
+        }
+
+        /// <summary>
+        /// Returns player nickname constrained to maximum allowed length and with invalid characters removed.
+        /// Does not check for offensive words or invalid first characters for CnCNet.
+        /// </summary>
+        /// <param name="name">Player nickname.</param>
+        /// <returns>Player nickname with invalid characters removed and constrained to maximum name length.</returns>
+        public static string GetValidOfflineName(string name)
+        {
+            string validName = new string(name.Trim().Where(c => ALLOWED_CHARS.Contains(c)).ToArray());
+
+            if (validName.Length > ClientConfiguration.Instance.MaxNameLength)
+                return validName.Substring(0, ClientConfiguration.Instance.MaxNameLength);
+            
+            return validName;
         }
     }
 }
