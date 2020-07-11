26ace49e56bcb2a61960bdba3fd9bece80be8442
Starkku <starkku.ppmod@gmail.com>
2020-07-11 18:48:38 +0300

    Make invalid characters check for offline nicknames less strict.

--- ClientCore/CnCNet5/NameValidator.cs
+++ ClientCore/CnCNet5/NameValidator.cs
@@ -5,8 +5,6 @@ namespace ClientCore.CnCNet5
 {
     public static class NameValidator
     {
-        private static readonly char[] ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_[]|\\{}^`".ToCharArray();
-
         /// <summary>
         /// Checks if the player's nickname is valid for CnCNet.
         /// </summary>
@@ -29,11 +27,12 @@ namespace ClientCore.CnCNet5
                 return "The first character in the player name cannot be a dash ( - ).";
 
             // Check that there are no invalid chars
+            char[] allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_[]|\\{}^`".ToCharArray();
             char[] nicknameChars = name.ToCharArray();
 
             foreach (char nickChar in nicknameChars)
             {
-                if (!ALLOWED_CHARS.Contains(nickChar))
+                if (!allowedCharacters.Contains(nickChar))
                 {
                     return "Your player name has invalid characters in it." + Environment.NewLine +
                     "Allowed characters are anything from A to Z and numbers.";
@@ -47,14 +46,16 @@ namespace ClientCore.CnCNet5
         }
 
         /// <summary>
-        /// Returns player nickname constrained to maximum allowed length and with invalid characters removed.
-        /// Does not check for offensive words or invalid first characters for CnCNet.
+        /// Returns player nickname constrained to maximum allowed length and with invalid characters for offline nicknames removed.
+        /// Does not check for offensive words or invalid characters for CnCNet.
         /// </summary>
         /// <param name="name">Player nickname.</param>
-        /// <returns>Player nickname with invalid characters removed and constrained to maximum name length.</returns>
+        /// <returns>Player nickname with invalid offline nickname characters removed and constrained to maximum name length.</returns>
         public static string GetValidOfflineName(string name)
         {
-            string validName = new string(name.Trim().Where(c => ALLOWED_CHARS.Contains(c)).ToArray());
+            char[] disallowedCharacters = ",;".ToCharArray();
+
+            string validName = new string(name.Trim().Where(c => !disallowedCharacters.Contains(c)).ToArray());
 
             if (validName.Length > ClientConfiguration.Instance.MaxNameLength)
                 return validName.Substring(0, ClientConfiguration.Instance.MaxNameLength);
