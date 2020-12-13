c0e6c56361ad386bb76df44f58220f582b483f8a
Meta (X230T) <crabiter@vivaldi.net>
2020-12-13 13:29:38 +0200

    Update XNAUI and Tools
    Closes #202.
    Closes #199.

--- DXMainClient/Properties/AssemblyInfo.cs
+++ DXMainClient/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.7.5")]
-[assembly: AssemblyFileVersion("2.6.7.5")]
+[assembly: AssemblyVersion("2.6.7.6")]
+[assembly: AssemblyFileVersion("2.6.7.6")]
