b5101805cc587572f7b2141fc02d6a5f0fe68ce2
Rampastring <rampastring@outlook.com>
2021-02-23 23:17:03 +0200

    Add ReadTranslationAttributes to XNAWindowBase

--- ClientGUI/XNAWindowBase.cs
+++ ClientGUI/XNAWindowBase.cs
@@ -5,6 +5,7 @@ using Rampastring.XNAUI.XNAControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ClientGUI
@@ -65,6 +66,61 @@ namespace ClientGUI
             {
                 if (!(typeof(XNAWindowBase).IsAssignableFrom(child.GetType())))
                     child.GetAttributes(iniFile);
+            }
+        }
+
+        public void ReadTranslationAttributes(IniSection translationIniSection)
+        {
+            foreach (XNAControl child in Children)
+            {
+                Control_ReadAttributesRecursive(Name, child, translationIniSection);
+            }
+        }
+
+        private List<Type> allowedPropertyTypes = new List<Type>() { typeof(int), typeof(string), typeof(bool), typeof(double) };
+
+        private void Control_ReadAttributesRecursive(string controlNameBase, XNAControl control, IniSection translationIniSection)
+        {
+            var controlType = control.GetType();
+            var properties = controlType.GetProperties();
+            foreach (var property in properties)
+            {
+                if (!allowedPropertyTypes.Contains(property.PropertyType))
+                    continue;
+
+                var setter = property.SetMethod;
+                if (setter == null)
+                    continue;
+
+                var getter = property.GetMethod;
+                if (getter == null)
+                    continue;
+
+                var propertyKey = controlNameBase + "_" + control.Name + "_" + property.Name;
+                if (!translationIniSection.KeyExists(propertyKey))
+                    continue;
+
+                switch (property.PropertyType)
+                {
+                    case Type t when t == typeof(int):
+                        property.SetValue(control, translationIniSection.GetIntValue(propertyKey, (int)property.GetValue(control)));
+                        break;
+                    case Type t when t == typeof(string):
+                        property.SetValue(control, translationIniSection.GetStringValue(propertyKey, (string)property.GetValue(control)));
+                        break;
+                    case Type t when t == typeof(bool):
+                        property.SetValue(control, translationIniSection.GetBooleanValue(propertyKey, (bool)property.GetValue(control)));
+                        break;
+                    case Type t when t == typeof(double):
+                        property.SetValue(control, translationIniSection.GetDoubleValue(propertyKey, (double)property.GetValue(control)));
+                        break;
+                }
+            }
+
+            controlNameBase = controlNameBase + "_" + control.Name;
+            foreach (XNAControl child in control.Children)
+            {
+                Control_ReadAttributesRecursive(controlNameBase, child, translationIniSection);
             }
         }
 
