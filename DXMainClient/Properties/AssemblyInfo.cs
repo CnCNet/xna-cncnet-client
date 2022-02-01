3873ee4ac7f200fd4f5ae8544616d2cc9e2835a2
Kerbiter <crabiter@vivaldi.net>
2022-02-01 17:38:28 +0200

    Bump version

--- DXMainClient/Properties/AssemblyInfo.cs
+++ DXMainClient/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.6.11.0")]
-[assembly: AssemblyFileVersion("2.6.11.0")]
+[assembly: AssemblyFileVersion("2.6.11.1")]
