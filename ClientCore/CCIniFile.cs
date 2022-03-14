d9c0a5923b8f90bcbba5dc990530036a68a5ec56
devo1929 <devo1929@gmail.com>
2022-03-14 06:23:39 -0400

    Make BasedOn a list

--- ClientCore/CCIniFile.cs
+++ ClientCore/CCIniFile.cs
@@ -36,23 +36,33 @@ namespace ClientCore
 
         protected override void ApplyBaseIni()
         {
-            string basedOn = GetStringValue("INISystem", "BasedOn", string.Empty);
-            if (!string.IsNullOrEmpty(basedOn))
-            {
-                string path;
-                if (basedOn.Contains("$THEME_DIR$"))
-                    path = basedOn.Replace("$THEME_DIR$", ProgramConstants.GetResourcePath().TrimEnd(new char[] { '/', '\\' }));
-                else
-                    path = Path.GetDirectoryName(FileName) + "/" + basedOn;
+            string basedOnSetting = GetStringValue("INISystem", "BasedOn", string.Empty);
+            if (string.IsNullOrEmpty(basedOnSetting))
+                return;
 
-                // Consolidate with the INI file that this INI file is based on
-                if (!File.Exists(path))
-                    Logger.Log(FileName + ": Base INI file not found! " + path);
+            string[] basedOns = basedOnSetting.Split(',');
+            foreach (string basedOn in basedOns)
+                ApplyBasedOnIni(basedOn);
+        }
 
-                CCIniFile baseIni = new CCIniFile(path);
-                ConsolidateIniFiles(baseIni, this);
-                Sections = baseIni.Sections;
-            }
+        private void ApplyBasedOnIni(string basedOn)
+        {
+            if (string.IsNullOrEmpty(basedOn))
+                return;
+            
+            string path;
+            if (basedOn.Contains("$THEME_DIR$"))
+                path = basedOn.Replace("$THEME_DIR$", ProgramConstants.GetResourcePath().TrimEnd(new char[] { '/', '\\' }));
+            else
+                path = Path.GetDirectoryName(FileName) + "/" + basedOn;
+
+            // Consolidate with the INI file that this INI file is based on
+            if (!File.Exists(path))
+                Logger.Log(FileName + ": Base INI file not found! " + path);
+
+            CCIniFile baseIni = new CCIniFile(path);
+            ConsolidateIniFiles(baseIni, this);
+            Sections = baseIni.Sections;
         }
     }
 }
