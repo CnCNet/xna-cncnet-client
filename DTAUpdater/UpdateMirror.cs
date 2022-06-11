6511784496b993914af8de771ad603e65031e6a8
Rans4ckeR <ransacker@outlook.com>
2022-06-12 00:34:22 +0200

    Include updater

---
+++ DTAUpdater/UpdateMirror.cs
@@ -0,0 +1,22 @@
+﻿namespace Updater
+{
+    public class UpdateMirror
+    {
+        public string Url { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public UpdateMirror()
+        {
+        }
+
+        public UpdateMirror(string url, string name, string location)
+        {
+            Url = url;
+            Name = name;
+            Location = location;
+        }
+    }
+}
