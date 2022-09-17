6b2bb8bb63f062ded5f5085b0fa8ce0e9cea2298
Rans4ckeR <ransacker@outlook.com>
2022-09-18 01:31:16 +0200

    Fix build output path

--- BuildScripts/Build-Ares-net48.ps1
+++ BuildScripts/Build-Ares-net48.ps1
@@ -5,9 +5,9 @@ Param([Parameter(Mandatory=$false)] [string] $Configuration = "Release")
 
 $path = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsXNA -f net48 -a x86 -o ..\Compiled\Ares\net48\Resources\Binaries\XNA
+dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsXNA -f net48 -a x86 -o $path\..\Compiled\Ares\net48\Resources\Binaries\XNA
 if ($LASTEXITCODE) { throw }
-dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsDX -f net48 -o ..\Compiled\Ares\net48\Resources\Binaries\Windows
+dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsDX -f net48 -o $path\..\Compiled\Ares\net48\Resources\Binaries\Windows
 if ($LASTEXITCODE) { throw }
-dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsGL -f net48 -o ..\Compiled\Ares\net48\Resources\Binaries\OpenGL
+dotnet publish $path\..\DXMainClient\DXMainClient.csproj -c $Configuration -p:GAME=Ares -p:ENGINE=WindowsGL -f net48 -o $path\..\Compiled\Ares\net48\Resources\Binaries\OpenGL
 if ($LASTEXITCODE) { throw }
