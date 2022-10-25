ace4fd56c72679a1cf29fcae183bcb2e5b181a6e
Rampastring <rampastring@outlook.com>
2022-10-26 01:06:51 +0300

    Set CampaignID= when launching missions + don't load multiple Battle*.ini files anymore

--- DXMainClient/Domain/Mission.cs
+++ DXMainClient/Domain/Mission.cs
@@ -8,8 +8,9 @@ namespace DTAClient.Domain
     /// </summary>
     public class Mission
     {
-        public Mission(IniFile iniFile, string sectionName)
+        public Mission(IniFile iniFile, string sectionName, int index)
         {
+            Index = index;
             CD = iniFile.GetIntValue(sectionName, nameof(CD), 0);
             Side = iniFile.GetIntValue(sectionName, nameof(Side), 0);
             Scenario = iniFile.GetStringValue(sectionName, nameof(Scenario), string.Empty);
@@ -25,6 +26,7 @@ namespace DTAClient.Domain
             GUIDescription = GUIDescription.Replace("@", Environment.NewLine);
         }
 
+        public int Index { get; }
         public int CD { get; }
         public int Side { get; }
         public string Scenario { get; }
