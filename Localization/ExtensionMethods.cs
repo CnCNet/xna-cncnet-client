0262b4f8dc117b26099f20dfa8cd6b932a690138
Sad Pencil <qh06@qq.com>
2022-11-16 10:20:58 +0800

    Format Localization project to suppress warnings

--- Localization/ExtensionMethods.cs
+++ Localization/ExtensionMethods.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 
-namespace Localization
+namespace Localization;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "To explicitly define all extension methods in a single file.")]
+public static class StringTranslationLabelExtensions
 {
-    public static class StringTranslationLabelExtensions
-    {
-        /// <summary>
-        /// Mark this string to be translated, with the given label name.
-        /// </summary>
-        /// <param name="defaultValue">The default string value as a fallback.</param>
-        /// <param name="label">The unique label name.</param>
-        /// <returns>The translated string value.</returns>
-        public static string L10N(this String defaultValue, string label)
-            => TranslationTable.Instance.GetTableValue(label, defaultValue);
-    }
+    /// <summary>
+    /// Mark this string to be translated, with the given label name.
+    /// </summary>
+    /// <param name="defaultValue">The default string value as a fallback.</param>
+    /// <param name="label">The unique label name.</param>
+    /// <returns>The translated string value.</returns>
+    public static string L10N(this String defaultValue, string label)
+        => TranslationTable.Instance.GetTableValue(label, defaultValue);
 }
--- Localization/MissingTranslationEventArgs.cs
+++ Localization/MissingTranslationEventArgs.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 
-namespace Localization
-{
-    public class MissingTranslationEventArgs : EventArgs
-    {
-        public string LanguageTag { get; set; }
-        public string Label { get; set; }
-        public string DefaultValue { get; set; }
+namespace Localization;
 
-        public MissingTranslationEventArgs(string languageTag, string label, string defaultValue)
-        {
-            LanguageTag = languageTag;
-            Label = label;
-            DefaultValue = defaultValue;
-        }
+public class MissingTranslationEventArgs : EventArgs
+{
+    public string LanguageTag { get; set; }
+    public string Label { get; set; }
+    public string DefaultValue { get; set; }
+
+    public MissingTranslationEventArgs(string languageTag, string label, string defaultValue)
+    {
+        LanguageTag = languageTag;
+        Label = label;
+        DefaultValue = defaultValue;
     }
 }
