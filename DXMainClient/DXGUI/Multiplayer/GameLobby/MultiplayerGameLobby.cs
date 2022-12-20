7cc06e4dbdeae916829ec4cca05d65a5b3342c11
Rans4ckeR <ransacker@outlook.com>
2022-12-20 11:33:15 +0100

    Don't parse statistics on UI thread

--- DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Rampastring.XNAUI;
-using Rampastring.XNAUI.XNAControls;
 using Microsoft.Xna.Framework;
 using ClientCore;
 using System.IO;
