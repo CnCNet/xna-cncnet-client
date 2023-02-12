2af84e3add1dbae7c813d0d19ca1b7d0eac7f810
Rans4ckeR <ransacker@outlook.com>
2023-02-12 22:04:19 +0100

    Custom loading screen path is incorrect #439 (#441)

--- ClientCore/LoadingScreenController.cs
+++ ClientCore/LoadingScreenController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Rampastring.Tools;
 
 namespace ClientCore
 {
@@ -7,21 +8,19 @@ namespace ClientCore
         public static string GetLoadScreenName(string sideId)
         {
             int resHeight = UserINISettings.Instance.IngameScreenHeight;
-
-            string loadingScreenName = ProgramConstants.BASE_RESOURCE_PATH + "l";
+            int randomInt = new Random().Next(1, 1 + ClientConfiguration.Instance.LoadingScreenCount);
+            string resolutionText;
 
             if (resHeight < 480)
-                loadingScreenName += "400";
+                resolutionText = "400";
             else if (resHeight < 600)
-                loadingScreenName += "480";
+                resolutionText = "480";
             else
-                loadingScreenName += "600";
+                resolutionText = "600";
 
-            loadingScreenName = loadingScreenName + "s" + sideId;
-            Random random = new Random();
-            int randomInt = random.Next(1, 1 + ClientConfiguration.Instance.LoadingScreenCount);
-
-            return loadingScreenName + Convert.ToString(randomInt) + ".pcx";
+            return SafePath.CombineFilePath(
+                ProgramConstants.BASE_RESOURCE_PATH,
+                FormattableString.Invariant($"l{resolutionText}s{sideId}{randomInt}.pcx")).Replace('\\', '/');
         }
     }
 }
