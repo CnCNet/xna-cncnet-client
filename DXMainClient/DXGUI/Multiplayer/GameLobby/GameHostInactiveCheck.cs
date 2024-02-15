012fdf5a97c27e1819eb48cf483d99732b6c51c5
devo1929 <devo1929@gmail.com>
2024-02-15 14:47:50 -0500

    Consolidate inactive timer check, move logic into containing class

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameHostInactiveCheck.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameHostInactiveCheck.cs
@@ -1,54 +1,81 @@
-﻿using ClientCore;
-using Rampastring.Tools;
-using System;
+﻿using System;
+using System.Timers;
+using ClientCore;
+using ClientGUI;
+using Rampastring.XNAUI;
 
 namespace DTAClient.DXGUI.Multiplayer.GameLobby
 {
-    class GameHostInactiveCheck
+    public class GameHostInactiveCheck
     {
-        private int secondsElapsed;
-        private bool inactiveGameWarningMessageSent = false;
-        private bool closeInactiveGameEventSent = false;
+        private readonly WindowManager windowManager;
+        private readonly Timer timer;
+        private bool warningShown;
+        private DateTime startDttm;
+        private static int WarningSeconds => ClientConfiguration.Instance.InactiveHostWarningMessageSeconds;
+        private static int CloseSeconds => WarningSeconds + ClientConfiguration.Instance.InactiveHostKickSeconds;
 
-        public event EventHandler SendInactiveGameWarningMessage;
-        public event EventHandler CloseInactiveGame;
+        public event EventHandler CloseEvent;
+
+        public GameHostInactiveCheck(WindowManager windowManager)
+        {
+            this.windowManager = windowManager;
+            timer = CreateTimer();
+        }
+
+
+        private Timer CreateTimer()
+        {
+            var _timer = new Timer();
+            _timer.AutoReset = true;
+            _timer.Interval = 1000;
+            _timer.Elapsed += TimerOnElapsed;
+            return _timer;
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+        {
+            double secondsElapsed = (DateTime.UtcNow - startDttm).TotalSeconds;
+
+            if (secondsElapsed > WarningSeconds && !warningShown)
+                ShowWarning();
+
+            if (secondsElapsed > CloseSeconds)
+                SendCloseEvent();
+        }
 
         public void Start()
         {
-            secondsElapsed++;
-
-            if (secondsElapsed > ClientConfiguration.Instance.InactiveHostWarningMessageSeconds &&
-                !inactiveGameWarningMessageSent)
-            {
-                SendHostInactiveEvent();
-            }
-
-            if (secondsElapsed > ClientConfiguration.Instance.InactiveHostKickSeconds &&
-                inactiveGameWarningMessageSent &&
-                !closeInactiveGameEventSent)
-            {
-                SendCloseGameEvent();
-            }
+            Reset();
+            timer.Start();
         }
+
 
         public void Reset()
         {
-            secondsElapsed = 0;
-            closeInactiveGameEventSent = false;
-            inactiveGameWarningMessageSent = false;
+            startDttm = DateTime.UtcNow;
+            warningShown = false;
         }
 
-        private void SendCloseGameEvent()
+        public void Stop() => timer.Stop();
+
+        private void SendCloseEvent()
         {
-            CloseInactiveGame?.Invoke(this, null);
-            closeInactiveGameEventSent = true;
+            Stop();
+            CloseEvent?.Invoke(this, null);
         }
 
-        private void SendHostInactiveEvent()
+        private void ShowWarning()
         {
-            SendInactiveGameWarningMessage?.Invoke(this, null);
-            inactiveGameWarningMessageSent = true;
-            secondsElapsed = 0;
+            warningShown = true;
+            XNAMessageBox hostInactiveWarningMessageBox = new XNAMessageBox(
+                windowManager,
+                ClientConfiguration.Instance.InactiveHostWarningTitle,
+                ClientConfiguration.Instance.InactiveHostWarningMessage,
+                XNAMessageBoxButtons.OK
+            );
+            hostInactiveWarningMessageBox.OKClickedAction = box => Reset();
+            hostInactiveWarningMessageBox.Show();
         }
     }
 }
