754bd0a433df409032b585f67a71747a5b6b1334
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-03-08 19:31:19 +0800

    Refactor Build Scripts
    Signed-off-by: 舰队的偶像-岛风酱! <frg2089@outlook.com>

---
+++ build.ps1
@@ -0,0 +1,130 @@
+#!/usr/bin/env pwsh
+#Requires -Version 5.1
+
+<#
+.SYNOPSIS
+  Build Project.
+.DESCRIPTION
+  You can used this script to make publish packages for your game.
+.PARAMETER Games
+  What game would you want to build.
+  Build all games when they empty.
+.PARAMETER IsDebug
+  Build projects by debug mode.
+.EXAMPLE
+  build.ps1
+  Build for all games.
+.EXAMPLE
+  build.ps1 Ares
+  Build for ares game.
+.EXAMPLE
+  build.ps1 Ares -IsDebug
+  Build for ares game on debug mode.
+#>
+param(
+  [Parameter(ValueFromRemainingArguments)]
+  [string[]]
+  $Games,
+  [Parameter()]
+  [switch]
+  $IsDebug
+)
+
+$Script:ConfigurationSuffix = 'Release'
+if ($IsDebug) {
+  $Script:ConfigurationSuffix = 'Debug'
+}
+
+$Script:RepoRoot = $PSScriptRoot
+$Script:ProjectPath = Join-Path $RepoRoot 'DXMainClient' 'DXMainClient.csproj'
+$Script:CompiledRoot = Join-Path $RepoRoot 'Compiled'
+$Script:EngineSubFolderMap = @{
+  'UniversalGL' = 'UniversalGL'
+  'WindowsDX'   = 'Windows'
+  'WindowsGL'   = 'OpenGL'
+  'WindowsXNA'  = 'XNA'
+}
+$Script:FrameworkBinariesFolderMap = @{
+  'net48'          = 'Binaries'
+  'net8.0'         = 'BinariesNET8'
+  'net8.0-windows' = 'BinariesNET8'
+}
+
+if ($null -EQ $IsWindows -AND 'Desktop' -EQ $PSEdition ) {
+  $Script:IsWindows = $true
+}
+
+function Script:Invoke-BuildProject {
+  [CmdletBinding(DefaultParameterSetName = 'ByGame')]
+  param (
+    [Parameter(ParameterSetName = 'ByGame', Position = 0)]
+    [Parameter(Mandatory, ParameterSetName = 'Detail', Position = 0)]
+    [string]
+    $Game,
+    [Parameter(Mandatory, ParameterSetName = 'Detail')]
+    [string]
+    $Engine,
+    [Parameter(Mandatory, ParameterSetName = 'Detail')]
+    [string]
+    $Framework
+  )
+  
+  process {
+    if ($Engine) {
+      $Output = Join-Path $CompiledRoot $Game $Output 'Resources' ($FrameworkBinariesFolderMap[$Framework]) ($EngineSubFolderMap[$Engine])
+  
+      $Private:ArgumentList = [System.Collections.Generic.List[string]]::new(11)
+      $Private:ArgumentList.Add('publish')
+      $Private:ArgumentList.Add("$ProjectPath")
+      $Private:ArgumentList.Add('--graph')
+      $Private:ArgumentList.Add("--configuration:${Game}${Engine}$Script:ConfigurationSuffix")
+      $Private:ArgumentList.Add("--framework:$Framework")
+      $Private:ArgumentList.Add("--output:$Output")
+      $Private:ArgumentList.Add('-property:SatelliteResourceLanguages=en')
+      # $Private:ArgumentList.Add("-property:AssemblyVersion=$AssemblySemVer")
+      # $Private:ArgumentList.Add("-property:FileVersion=$AssemblySemFileVer")
+      # $Private:ArgumentList.Add("-property:InformationalVersion=$InformationalVersion")
+  
+      if ($Engine -eq 'WindowsXNA') {
+        $Private:ArgumentList.Add('--arch=x86')
+      }
+  
+      $Private:Process = Start-Process `
+        -FilePath 'dotnet' `
+        -ArgumentList $Private:ArgumentList `
+        -NoNewWindow `
+        -PassThru `
+        -Wait
+  
+      if ($Private:Process.ExitCode -NE 0) {
+        throw "Build failed for ${Game}${Engine}$Script:ConfigurationSuffix $Framework"
+      }
+    }
+    elseif ($Game) {
+      Invoke-BuildProject -Game $Game -Engine 'UniversalGL' -Framework 'net8.0'
+      if ($IsWindows) {
+        @('WindowsDX', 'WindowsGL', 'WindowsXNA') | ForEach-Object {
+          $Private:Engine = $PSItem
+  
+          @('net48', 'net8.0-windows') | ForEach-Object {
+            $Private:Framework = $PSItem
+  
+            Invoke-BuildProject -Game $Game -Engine $Private:Engine -Framework $Private:Framework
+          }
+        }
+      }
+    }
+    else {
+      @('Ares', 'TS', 'YR') | ForEach-Object {
+        Invoke-BuildProject -Game $PSItem
+      }
+    }
+  }
+}
+
+if ($Games.Count -EQ 0) {
+  Script:Invoke-BuildProject
+}
+else {
+  $Games | ForEach-Object { Script:Invoke-BuildProject $PSItem }
+}
