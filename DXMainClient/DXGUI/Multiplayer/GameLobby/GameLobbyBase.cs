01f6ef40477cb9665036b45586e6fb300e0198e1
Flactine <1716455702@qq.com>
2024-11-24 23:56:02 +0800

    Update GameLobbyBase.cs
    When the number of players is 1, the random map will be randomly assigned to any map, regardless of the number of players. Used to randomly check maps.

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -742,6 +742,11 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
 
         private List<Map> GetMapList(int playerCount)
         {
+            if (playerCount == 1)
+			{
+				List<Map> allMaps = GameMode?.Maps.ToList() ?? new List<Map>();
+				return allMaps;
+			}
             List<Map> mapList = (GameMode?.Maps.Where(x => x.MaxPlayers == playerCount) ?? Array.Empty<Map>()).ToList();
             if (mapList.Count < 1 && playerCount <= MAX_PLAYER_COUNT)
                 return GetMapList(playerCount + 1);
