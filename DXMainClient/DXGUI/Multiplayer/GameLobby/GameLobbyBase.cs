6349643ffc86faccbc07c66f5013490a3cef0881
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-11-25 20:14:10 +0300

    Fix text and add new lines
    Co-authored-by: Metadorius <Metadorius@users.noreply.github.com>

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -208,7 +208,7 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
             }
             catch (Exception ex)
             {
-                throw new Exception(string.Format("It seems the modders have not migrated the 'Tiberian Sun Client v6 Changes'. Please refer to {0} for more details. Error message: {1}".L10N("Client:Main:NotMigratedClientException"),
+                throw new Exception(string.Format("It seems the client configuration was not migrated to accomodate for the 'Tiberian Sun Client v6 Changes'.@Please refer to {0} for more details.@@Error message: {1}".L10N("Client:Main:NotMigratedClientException").Replace("@", Environment.NewLine),
                                                   "https://github.com/CnCNet/xna-cncnet-client/blob/122b2de962afc404e203290d0618363d83c4264a/Docs/Migration-INI.md",
                                                    ex.Message));
             }
