a0c355b565858531188724c44b79c93afcdf5937
Grant Bartlett <hello@grant-bartlett.com>
2025-01-02 12:03:26 +0000

    Remove ref in BaseGameLobby

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -74,7 +74,6 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
         public List<GameLobbyDropDown> DropDowns = new List<GameLobbyDropDown>();
 
         protected DiscordHandler discordHandler;
-        protected PrivateMessagingWindow pmWindow;
 
         protected MapLoader MapLoader;
         /// <summary>
