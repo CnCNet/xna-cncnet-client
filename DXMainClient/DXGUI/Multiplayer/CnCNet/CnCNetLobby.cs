cb6f0d61c67eb5da7c542d7b4d7b125b6f72b0fe
Grant Bartlett <hello@grant-bartlett.com>
2025-01-03 22:51:22 +0000

    Update version text

--- DXMainClient/DXGUI/Multiplayer/CnCNet/CnCNetLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/CnCNet/CnCNetLobby.cs
@@ -557,7 +557,8 @@ namespace DTAClient.DXGUI.Multiplayer.CnCNet
 
             gameCreationPanel.Hide();
 
-            string clientVersion = GitVersionInformation.AssemblySemVer;
+            //string clientVersion = GitVersionInformation.AssemblySemVer;
+            string clientVersion = "yr/develop, based on the XNA Client Version 2.11.6.0";
 #if DEVELOPMENT_BUILD
             clientVersion = $"{GitVersionInformation.CommitDate} {GitVersionInformation.BranchName}@{GitVersionInformation.ShortSha}";
 #endif
