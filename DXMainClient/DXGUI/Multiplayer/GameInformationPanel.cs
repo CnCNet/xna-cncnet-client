cf2368997888d875f357b687c2b43e62070910bd
Grant Bartlett <hello@grant-bartlett.com>
2025-01-04 13:07:57 +0000

    Fix height of game info panel

--- DXMainClient/DXGUI/Multiplayer/GameInformationPanel.cs
+++ DXMainClient/DXGUI/Multiplayer/GameInformationPanel.cs
@@ -37,7 +37,7 @@ namespace DTAClient.DXGUI.Multiplayer
 
         public override void Initialize()
         {
-            ClientRectangle = new Rectangle(0, 0, 235, 264);
+            ClientRectangle = new Rectangle(0, 0, 235, 288);
             BackgroundTexture = AssetLoader.CreateTexture(new Color(0, 0, 0, 255), 1, 1);
             PanelBackgroundDrawMode = PanelBackgroundImageDrawMode.STRETCHED;
 
