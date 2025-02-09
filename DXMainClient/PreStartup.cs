22666105f72eb95532058cc4b25817e0ed60020d
[11EJ11] <jacobseliot54@gmail.com>
2025-02-10 10:48:19 +1300

    Fix style

--- DXMainClient/PreStartup.cs
+++ DXMainClient/PreStartup.cs
@@ -90,7 +90,6 @@ namespace DTAClient
 
             Logger.Log("***Logfile for " + MainClientConstants.GAME_NAME_LONG + " client***");
 
-
             string clientVersion = GitVersionInformation.AssemblySemVer;
 #if DEVELOPMENT_BUILD
             clientVersion = $"{GitVersionInformation.CommitDate} {GitVersionInformation.BranchName}@{GitVersionInformation.ShortSha}";
