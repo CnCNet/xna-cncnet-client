9af8f20a5411a357b737892b3970a25cebab1cbd
SadPencil <sadpencil@outlook.com>
2025-05-04 21:50:25 +0800

    Add asserts on whether a map file path ends with the extension

--- DXMainClient/Domain/Multiplayer/Map.cs
+++ DXMainClient/Domain/Multiplayer/Map.cs
@@ -49,6 +49,8 @@ namespace DTAClient.Domain.Multiplayer
 
         public Map(string baseFilePath, bool isCustomMap)
         {
+            Debug.Assert(!baseFilePath.EndsWith(ClientConfiguration.Instance.MapFileExtension, StringComparison.InvariantCultureIgnoreCase), $"Unexpected map path {baseFilePath}. It should not end with the map extension.");
+
             BaseFilePath = baseFilePath;
             customMapFilePath = isCustomMap
                 ? SafePath.CombineFilePath(ProgramConstants.GamePath, FormattableString.Invariant($"{baseFilePath}.{ClientConfiguration.Instance.MapFileExtension}"))
