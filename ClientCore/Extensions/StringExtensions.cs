6dcc40cce9228d817823bc90ccc8910bcde3f633
SadPencil <sadpencil@outlook.com>
2025-05-04 16:40:08 +0800

    Use a HashSet for a  case-insensitive comparison

--- ClientCore/Extensions/StringExtensions.cs
+++ ClientCore/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 using ClientCore.I18N;
 
 namespace ClientCore.Extensions;
@@ -86,33 +88,26 @@ public static class StringExtensions
     /// <remarks>
     /// Reference: <a href="https://learn.microsoft.com/en-us/windows/win32/fileio/naming-a-file">Naming Files, Paths, and Namespaces</a>.
     /// </remarks>
-    public static string ToWin32FileName(this string defaultValue)
+    public static string ToWin32FileName(this string filename)
     {
         foreach (char ch in "/\\:*?<>|")
-            defaultValue = defaultValue.Replace(ch, ' ');
+            filename = filename.Replace(ch, ' ');
 
         // If the user is somehow using "con" or any other filename that is
         // reserved by WIN32API, it would be better to rename it.
 
-        string[] reservedFileNames = 
-        {
+        HashSet<string> reservedFileNames = new HashSet<string>(new List<string>(){
             "CON",
             "PRN",
             "AUX",
             "NUL",
             "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "COM¹", "COM²", "COM³",
             "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9", "LPT¹", "LPT²", "LPT³"
-        };
+        }, StringComparer.InvariantCultureIgnoreCase);
 
-        foreach(var reserved in reservedFileNames)
-        {
-            if (defaultValue == reserved)
-            {
-                defaultValue += "_map";
-                break;
-            }
-        }
+        if (reservedFileNames.Contains(filename))
+            filename += "_map";
 
-        return defaultValue;
+        return filename;
     }
 }
