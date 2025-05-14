51f4cb9fa03395081135937e837d6c141423f7d8
Metadorius <crabiter@vivaldi.net>
2025-05-14 20:54:15 +0300

    Refactor related enum code

--- ClientCore/Enums/ClientTypeHelper.cs
+++ ClientCore/Enums/ClientTypeHelper.cs
@@ -6,18 +6,18 @@ namespace ClientCore.Enums
 {
     public static class ClientTypeHelper
     {
-        private const ClientType ct = 0;
         public static ClientType FromString(string value) => value switch
         {
             "TS" => ClientType.TS,
             "YR" => ClientType.YR,
             "Ares" => ClientType.Ares,
-            _ => throw new Exception(string.Format(("It seems the client configuration was not migrated to accommodate for the v2.12 changes. " +
-                                                    "Please specify 'ClientGameType' in '[Settings]' section of the 'ClientDefinitions.ini' file " +
-                                                    "(allowed options: {0}).\n\n" +
-                                                    "Please refer to documentation of the client {1} for more details. This link can also be found in the log file.").L10N("Client:Main:ClientGameTypeNotFoundException"),
-                                                    ct.GetNames(),
-                                                    "https://github.com/CnCNet/xna-cncnet-client/")),
+            _ => throw new Exception(string.Format((
+                "It seems the client configuration was not migrated to accommodate for the v2.12 changes. " +
+                "Please specify 'ClientGameType' in '[Settings]' section of the 'ClientDefinitions.ini' file " +
+                "(allowed options: {0}).\n\n" +
+                "Please refer to documentation of the client {1} for more details. This link can also be found in the log file.").L10N("Client:Main:ClientGameTypeNotFoundException"),
+                EnumExtensions.GetNames<ClientType>(),
+                "https://github.com/CnCNet/xna-cncnet-client/")),
         };
     }
 }
