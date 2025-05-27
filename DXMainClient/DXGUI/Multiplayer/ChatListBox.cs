ed491568de1e9c3bc783a8b2ab66ff59863d1e9e
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-05-27 16:40:45 +0300

    Remove link from class field

--- DXMainClient/DXGUI/Multiplayer/ChatListBox.cs
+++ DXMainClient/DXGUI/Multiplayer/ChatListBox.cs
@@ -16,19 +16,18 @@ namespace DTAClient.DXGUI.Multiplayer
     /// </summary>
     public class ChatListBox : XNAListBox, IMessageView
     {
-        private string? link = null;
         public ChatListBox(WindowManager windowManager) : base(windowManager)
         {
             DoubleLeftClick += ChatListBox_DoubleLeftClick;
             MiddleClick += ChatListBox_MiddleClick;
         }
-
+        
         private void ChatListBox_DoubleLeftClick(object sender, EventArgs e)
         {
             if (SelectedIndex < 0 || SelectedIndex >= Items.Count)
                 return;
 
-            link = Items[SelectedIndex].Text?.GetLink();
+            var link = Items[SelectedIndex].Text?.GetLink();
             if (link == null)
                 return;
 
@@ -44,27 +43,22 @@ namespace DTAClient.DXGUI.Multiplayer
                 """.L10N("Client:Main:OpenLinkConfirmationText")
                 + Environment.NewLine + Environment.NewLine + link, 
                 XNAMessageBoxButtons.YesNo);
-            msgBox.YesClickedAction = OpenLinkConfirmation_YesClicked;
+            
+            msgBox.YesClickedAction = (msgBox) => ProcessLink(link);
 
             if (new Regex(ClientConfiguration.Instance.TrustedLinksRegExp).Match(link).Success)
             {
-                ProcessLink();
+                ProcessLink(link);
                 return;
             }
 
             msgBox.Show();
         }
 
-        private void OpenLinkConfirmation_YesClicked(XNAMessageBox messageBox)
-            => ProcessLink();
-
-        private void ProcessLink()
+        private void ProcessLink(string link)
         {
             if (link != null)
-            {
                 ProcessLauncher.StartShellProcess(link);
-                link = null;
-            }
         }
 
         private void ChatListBox_MiddleClick(object sender, EventArgs e)
@@ -72,12 +66,11 @@ namespace DTAClient.DXGUI.Multiplayer
             if (SelectedIndex < 0 || SelectedIndex >= Items.Count)
                 return;
 
-            link = Items[SelectedIndex].Text?.GetLink();
+            var link = Items[SelectedIndex].Text?.GetLink();
             if (link == null)
                 return;
 
             ProcessLauncher.StartShellProcess(link);
-            link = null;
         }
 
         public void AddMessage(string message)
