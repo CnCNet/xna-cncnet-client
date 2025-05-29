c07e8bbee974d28fcd1e2ca964afd2bb62a1b1f2
SadPencil <sadpencil@outlook.com>
2025-05-30 01:21:44 +0800

    Update the indent

--- DXMainClient/DXGUI/Multiplayer/ChatListBox.cs
+++ DXMainClient/DXGUI/Multiplayer/ChatListBox.cs
@@ -39,7 +39,7 @@ namespace DTAClient.DXGUI.Multiplayer
                 string domain = new Uri(link).Host;
                 var trustedDomains = ClientConfiguration.Instance.TrustedDomains.Concat(ClientConfiguration.Instance.AlwaysTrustedDomains);
                 isTrusted = trustedDomains.Contains(domain, StringComparer.InvariantCultureIgnoreCase)
-                || trustedDomains.Any(trustedDomain => domain.EndsWith("." + trustedDomain, StringComparison.InvariantCultureIgnoreCase));
+                    || trustedDomains.Any(trustedDomain => domain.EndsWith("." + trustedDomain, StringComparison.InvariantCultureIgnoreCase));
             }
             catch (Exception ex)
             {
