224bc77180ea786bb44b3a72d7e2b74f7de91c6b
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-06-27 14:55:26 +0300

    Add `ColorTextureWidth`, `ColorTextureHeigth`, `RandomColorTexture` keys

--- ClientGUI/XNAColorDropDown.cs
+++ ClientGUI/XNAColorDropDown.cs
@@ -1,5 +1,9 @@
-﻿using ClientCore.Extensions;
+﻿using System.Linq;
+
+using ClientCore.Extensions;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 using Rampastring.Tools;
 using Rampastring.XNAUI;
 using Rampastring.XNAUI.XNAControls;
@@ -13,48 +17,66 @@ namespace ClientGUI
 
         public int ColorTextureWidth;
         public int ColorTextureHeigth;
+        public Texture2D RandomColorTexture { get; private set; }
 
         public XNAColorDropDown(WindowManager windowManager) : base(windowManager) 
         {
             ColorTextureWidth = Height - PADDING;
             ColorTextureHeigth = Height - PADDING;
+            RandomColorTexture = AssetLoader.LoadTexture("randomicon.png");
         }
 
         protected override void ParseControlINIAttribute(IniFile iniFile, string key, string value)
         {
-            if (key == nameof(ItemsDrawMode))
+            switch (key)
             {
-                ItemsDrawMode = value.FromIniString().ToEnum<ItemsKind>();
+                case nameof(ItemsDrawMode):
+                    ItemsDrawMode = value.FromIniString().ToEnum<ItemsKind>();
 
-                switch (ItemsDrawMode)
-                {
-                    case ItemsKind.Text:
-                        Items.ForEach(item => 
-                        { 
-                            item.Texture = AssetLoader.CreateTexture(AssetLoader.GetRGBAColorFromString("0,0,0,0"), 1, 1); 
-                        });
-                        break;
-                    case ItemsKind.Icon:
-                        Items.ForEach(item => 
-                        {
-                            if (!item.Text.Contains("Random".L10N("Client:Main:RandomColor")))
-                                item.Texture = AssetLoader.CreateTexture(
-                                    item.TextColor ?? AssetLoader.GetColorFromString("255,255,255"), 
-                                    Width - PADDING, 
-                                    Height - PADDING);
-                            
-                            item.Text = string.Empty;
-                        });
-                        break;
-                    case ItemsKind.TextAndIcon:
-                    default:
-                        break;
-                }
+                    switch (ItemsDrawMode)
+                    {
+                        case ItemsKind.Text:
+                            Items.ForEach(item =>
+                            {
+                                // Disposing cause client to crash, so replace texture with transparent one
+                                item.Texture = AssetLoader.CreateTexture(AssetLoader.GetRGBAColorFromString("0,0,0,0"), 1, 1);
+                            });
+                            break;
+                        case ItemsKind.Icon:
+                            Items.ForEach(item =>
+                            {
+                                if (!item.Text.Contains("Random".L10N("Client:Main:RandomColor")))
+                                    item.Texture = AssetLoader.CreateTexture(
+                                        item.TextColor ?? AssetLoader.GetColorFromString("255,255,255"),
+                                        Width - PADDING,
+                                        Height - PADDING);
 
-                return;
+                                item.Text = string.Empty;
+                            });
+                            break;
+                        case ItemsKind.TextAndIcon:
+                        default:
+                            break;
+                    }
+
+                    return;
+                case nameof(ColorTextureWidth):
+                    ColorTextureWidth = Conversions.IntFromString(value, ColorTextureWidth);
+                    break;
+                case nameof(ColorTextureHeigth):
+                    ColorTextureHeigth = Conversions.IntFromString(value, ColorTextureHeigth);
+                    break;
+                case nameof(RandomColorTexture):
+                    RandomColorTexture = AssetLoader.LoadTexture(value);
+                    Items
+                        .Where(item => item.Text.Contains("Random".L10N("Client:Main:RandomColor")))
+                        .ToList()
+                        .ForEach(item => item.Texture = RandomColorTexture);
+                    break;
+                default:
+                    base.ParseControlINIAttribute(iniFile, key, value);
+                    return;
             }
-
-            base.ParseControlINIAttribute(iniFile, key, value);
         }
 
         public new virtual void AddItem(string text, Color color)
@@ -67,7 +89,7 @@ namespace ClientGUI
             if (!text.Contains("Random".L10N("Client:Main:RandomColor")))
                 item.Texture = AssetLoader.CreateTexture(color, ColorTextureWidth, ColorTextureHeigth);
             else
-                item.Texture = AssetLoader.LoadTexture("randomicon.png");
+                item.Texture = RandomColorTexture;
 
             Items.Add(item);
         }
