8e1bdc82269269ad4ac0c94677f9596fab6f214e
Metadorius <crabiter@vivaldi.net>
2025-07-04 17:51:57 +0300

    Adjust for the latest ScrollPanel version

--- ClientGUI/XNAClientScrollPanel.cs
+++ ClientGUI/XNAClientScrollPanel.cs
@@ -11,4 +11,14 @@ public class XNAClientScrollPanel : XNAScrollPanel, ICompositeControl
         => [ContentPanel, HorizontalScrollBar, VerticalScrollBar, CornerPanel];
     
     public XNAClientScrollPanel(WindowManager windowManager) : base(windowManager) { }
+    
+    protected override void ComposeControls()
+    {
+        // this is needed for the control composition to work properly, as otherwise
+        // the controls will be initialized twice via INItializableWindow system
+        AddChildWithoutInitialize(ContentPanel);
+        AddChildWithoutInitialize(HorizontalScrollBar);
+        AddChildWithoutInitialize(VerticalScrollBar);
+        AddChildWithoutInitialize(CornerPanel);
+    }
 }
