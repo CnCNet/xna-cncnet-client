6a1b8868104f43ce6610c7d08bb900d73085b0cf
[11EJ11] <jacobseliot54@gmail.com>
2025-10-04 07:46:23 +1300

    Move MapChangedEventArgs to new file

--- DXMainClient/Domain/Multiplayer/MapLoader.cs
+++ DXMainClient/Domain/Multiplayer/MapLoader.cs
@@ -15,20 +15,6 @@ using Rampastring.Tools;
 
 namespace DTAClient.Domain.Multiplayer
 {
-    public class MapChangedEventArgs : EventArgs
-    {
-        public Map Map { get; set; }
-        public MapChangeType ChangeType { get; set; }
-        public string PreviousMapSHA1 { get; set; }
-
-        public MapChangedEventArgs(Map map, MapChangeType changeType, string previousMapSHA1 = null)
-        {
-            Map = map;
-            ChangeType = changeType;
-            PreviousMapSHA1 = previousMapSHA1;
-        }
-    }
-
     public enum MapChangeType
     {
         Added,
