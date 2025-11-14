024f509bbd0acfa6747521c7c12e70c6f351d988
SadPencil <sadpencil@outlook.com>
2025-11-14 23:55:45 +0800

    Demonstrate C# 14 extension keyword

--- ClientCore/Extensions/IniFileExtensions.cs
+++ ClientCore/Extensions/IniFileExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 using Rampastring.Tools;
 
@@ -8,51 +7,58 @@ namespace ClientCore.Extensions
 {
     public static class IniFileExtensions
     {
-        // Clone() method is not officially available now. https://github.com/Rampastring/Rampastring.Tools/issues/12
-        public static IniFile Clone(this IniFile oldIniFile)
+        extension(IniFile iniFile)
         {
-            var newIni = new IniFile();
-            foreach (string sectionName in oldIniFile.GetSections())
+            // Clone() method is not officially available now. https://github.com/Rampastring/Rampastring.Tools/issues/12
+            public IniFile Clone()
             {
-                IniSection oldSection = oldIniFile.GetSection(sectionName);
-                newIni.AddSection(oldSection.Clone());
+                var newIni = new IniFile();
+                foreach (string sectionName in iniFile.GetSections())
+                {
+                    IniSection oldSection = iniFile.GetSection(sectionName);
+                    newIni.AddSection(oldSection.Clone());
+                }
+
+                return newIni;
             }
 
-            return newIni;
-        }
-
-        public static IniSection Clone(this IniSection oldSection)
-        {
-            IniSection newSection = new(oldSection.SectionName);
-
-            foreach ((var key, var value) in oldSection.Keys)
+            public IniSection GetOrAddSection(string sectionName)
             {
-                newSection.AddKey(key, value);
-            }
+                var section = iniFile.GetSection(sectionName);
+                if (section != null)
+                    return section;
 
-            return newSection;
-        }
-
-        public static IniSection GetOrAddSection(this IniFile iniFile, string sectionName)
-        {
-            var section = iniFile.GetSection(sectionName);
-            if (section != null)
+                section = new IniSection(sectionName);
+                iniFile.AddSection(section);
                 return section;
+            }
 
-            section = new IniSection(sectionName);
-            iniFile.AddSection(section);
-            return section;
+            public string[] GetStringListValue(string section, string key, string defaultValue, char[] separators = null)
+                => (iniFile.GetSection(section)?.GetStringValue(key, defaultValue) ?? defaultValue)
+                    .SplitWithCleanup();
+
         }
 
-        public static void RemoveAllKeys(this IniSection iniSection)
+        extension(IniSection iniSection)
         {
-            var keys = new List<KeyValuePair<string, string>>(iniSection.Keys);
-            foreach (KeyValuePair<string, string> iniSectionKey in keys)
-                iniSection.RemoveKey(iniSectionKey.Key);
-        }
+            public IniSection Clone()
+            {
+                IniSection newSection = new(iniSection.SectionName);
 
-        public static string[] GetStringListValue(this IniFile iniFile, string section, string key, string defaultValue, char[] separators = null)
-            => (iniFile.GetSection(section)?.GetStringValue(key, defaultValue) ?? defaultValue)
-                .SplitWithCleanup();
+                foreach ((var key, var value) in iniSection.Keys)
+                {
+                    newSection.AddKey(key, value);
+                }
+
+                return newSection;
+            }
+
+            public void RemoveAllKeys()
+            {
+                var keys = new List<KeyValuePair<string, string>>(iniSection.Keys);
+                foreach (KeyValuePair<string, string> iniSectionKey in keys)
+                    iniSection.RemoveKey(iniSectionKey.Key);
+            }
+        }
     }
 }
