430b81eabdc22fc7edff924eaa6d3fd065f1d5f8
11EJDE11 <jacobseliot54@gmail.com>
2025-12-26 03:57:52 +1300

    Add prompt on startup
    Update compat fixer to use correct executables
    
    Fix an issue applying the reg changes

--- DXMainClient/Domain/DirectDrawCompatibilityFixer.cs
+++ DXMainClient/Domain/DirectDrawCompatibilityFixer.cs
@@ -1,4 +1,5 @@
 #nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Versioning;
@@ -18,45 +19,61 @@ public static class DirectDrawCompatibilityFixer
     private static readonly IReadOnlyList<string> OSCompatibilityValues = [
         "WIN8RTM", "WIN7RTM", "VISTASP2", "VISTASP1", "VISTARTM", "WINXPSP3", "WINXPSP2", "WIN98", "WIN95"
     ];
-    
+
+    private static readonly IReadOnlyList<string> StaticExecutablesToCheck = [
+        "CnCNetYRLauncher.exe",
+        "gamemd.exe",
+        "gamemd-spawn.exe"
+    ];
+
+    private static IEnumerable<string> GetExecutablesToCheck()
+    {
+        // clientdx.exe, clientogl.exe, or clientxna.exe
+        string currentExeName = SafePath.GetFile(ProgramConstants.StartupExecutable).Name;
+
+        // static list plus the current executable
+        return StaticExecutablesToCheck.Append(currentExeName);
+    }
+
     public static void Examine(out bool requireFix, out bool requireAdmin)
     {
-        using RegistryKey? hkcuKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers");
-        using RegistryKey? hklmKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers");
-            
-        string gameExeFullPath = SafePath.CombineFilePath(ProgramConstants.GamePath,
-            ClientConfiguration.Instance.GetGameExecutableName());
-            
-        object? hkcuValue = hkcuKey?.GetValue(gameExeFullPath);
-        object? hklmValue = hklmKey?.GetValue(gameExeFullPath);
+        using RegistryKey? hkcuKey = Registry.CurrentUser.OpenSubKey(
+            @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers");
+        using RegistryKey? hklmKey = Registry.LocalMachine.OpenSubKey(
+            @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers");
 
-        bool IsFixRequired(object? regValue)
-            => regValue is string regValueString 
+        static bool IsFixRequired(object? regValue)
+            => regValue is string regValueString
                && regValueString.Split([' ']).Intersect(OSCompatibilityValues).Any();
 
-        bool hkcuRequireFix = IsFixRequired(hkcuValue);
-        bool hklmRequireFix = IsFixRequired(hklmValue);
+        bool anyHkcuRequireFix = false;
+        bool anyHklmRequireFix = false;
 
-        requireFix = hkcuRequireFix || hklmRequireFix;
-        requireAdmin = hklmRequireFix;
+        foreach (string executableName in GetExecutablesToCheck())
+        {
+            string exeFullPath = SafePath.CombineFilePath(ProgramConstants.GamePath, executableName);
+
+            object? hkcuValue = hkcuKey?.GetValue(exeFullPath);
+            object? hklmValue = hklmKey?.GetValue(exeFullPath);
+
+            if (IsFixRequired(hkcuValue))
+                anyHkcuRequireFix = true;
+
+            if (IsFixRequired(hklmValue))
+                anyHklmRequireFix = true;
+        }
+
+        requireFix = anyHkcuRequireFix || anyHklmRequireFix;
+        requireAdmin = anyHklmRequireFix;
     }
 
     public static void Fix()
     {
-        using RegistryKey hkcuKey = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers", writable: true);
-        using RegistryKey hklmKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers", writable: true);
-            
-        string gameExeFullPath = SafePath.CombineFilePath(ProgramConstants.GamePath,
-            ClientConfiguration.Instance.GetGameExecutableName());
-            
-        object? hkcuValue = hkcuKey.GetValue(gameExeFullPath);
-        object? hklmValue = hklmKey.GetValue(gameExeFullPath);
-
         void FixValue(object? regValue, out bool success, out string newRegValue)
         {
             if (regValue is string regValueString)
             {
-                newRegValue = string.Join(" ", 
+                newRegValue = string.Join(" ",
                     regValueString
                         .SplitWithCleanup(new [] {' '})
                         .Where(v => !OSCompatibilityValues.Contains(v)));
@@ -69,23 +86,40 @@ public static class DirectDrawCompatibilityFixer
             }
         }
 
-        FixValue(hkcuValue, out bool hkcuFixSuccess, out string newHkcuValue);
-        FixValue(hklmValue, out bool hklmFixSuccess, out string newHklmValue);
-
-        if (hkcuFixSuccess)
+        void FixRegistryKey(RegistryKey rootKey, string subKeyPath)
         {
-            if (string.IsNullOrEmpty(newHkcuValue))
-                hkcuKey.DeleteValue(gameExeFullPath, false);
-            else
-                hkcuKey.SetValue(gameExeFullPath, newHkcuValue, RegistryValueKind.String);
+            try
+            {
+                using RegistryKey? key = rootKey.OpenSubKey(subKeyPath, writable: true);
+                if (key == null)
+                    return;
+
+                foreach (string executableName in GetExecutablesToCheck())
+                {
+                    string exeFullPath = SafePath.CombineFilePath(ProgramConstants.GamePath, executableName);
+                    object? value = key.GetValue(exeFullPath);
+
+                    FixValue(value, out bool success, out string newValue);
+
+                    if (success)
+                    {
+                        if (string.IsNullOrEmpty(newValue))
+                            key.DeleteValue(exeFullPath, false);
+                        else
+                            key.SetValue(exeFullPath, newValue, RegistryValueKind.String);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log($"Failed to fix registry key {rootKey.Name}\\{subKeyPath}: {ex.Message}");
+            }
         }
 
-        if (hklmFixSuccess)
-        {
-            if (string.IsNullOrEmpty(newHklmValue))
-                hklmKey.DeleteValue(gameExeFullPath, false);
-            else
-                hklmKey.SetValue(gameExeFullPath, newHklmValue, RegistryValueKind.String);
-        }
+        string subKeyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers";
+
+        FixRegistryKey(Registry.CurrentUser, subKeyPath);
+
+        FixRegistryKey(Registry.LocalMachine, subKeyPath);
     }
 }
