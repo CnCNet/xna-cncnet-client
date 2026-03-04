89c04ca6ac8ca84e5b25cd8acf7db59d6b86f323
SadPencil <sadpencil@outlook.com>
2026-03-04 14:10:12 +0800

    Revert "Update GameLobbyBase.cs"
    This reverts commit 0c68c0510d2bde165619e640604080d79b2e568f.

--- DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/GameLobbyBase.cs
@@ -2571,10 +2571,11 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
             }
             else
             {
-                if (PlayerOptionsPanel == null) return;
-                if (PlayerExtraOptionsPanel == null) return;
-                PlayerExtraOptionsPanel.ForcedNoTeamsAllowChecking = true;
-                PlayerExtraOptionsPanel.UseTeamStartMappingsAllowChecking = true;
+                if (PlayerOptionsPanel != null)
+                {
+                    PlayerExtraOptionsPanel.ForcedNoTeamsAllowChecking = true;
+                    PlayerExtraOptionsPanel.UseTeamStartMappingsAllowChecking = true;
+                }
             }
 
             OnGameOptionChanged();
