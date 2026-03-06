2e042c3264417b0cbb431ead6ba93b220d1fcc12
CO2-code <majidabomehdi@gmail.com>
2026-03-06 21:56:40 +0200

    fixes :)

--- ClientCore/Settings/DoubleSetting.cs
+++ ClientCore/Settings/DoubleSetting.cs
@@ -4,9 +4,13 @@ namespace ClientCore.Settings
 {
     public class DoubleSetting : INISetting<double>
     {
+        private double _cachedValue;
+        public event EventHandler ValueChanged;
+
         public DoubleSetting(IniFile iniFile, string iniSection, string iniKey, double defaultValue)
             : base(iniFile, iniSection, iniKey, defaultValue)
         {
+            _cachedValue = Get();
         }
 
         protected override double Get()
@@ -16,6 +20,11 @@ namespace ClientCore.Settings
 
         protected override void Set(double value)
         {
+            if (_cachedValue != value)
+            {
+                _cachedValue = value;
+                ValueChanged?.Invoke(this, EventArgs.Empty);
+            }
             IniFile.SetDoubleValue(IniSection, IniKey, value);
         }
 
@@ -27,6 +36,12 @@ namespace ClientCore.Settings
         public override string ToString()
         {
             return Get().ToString();
+        }
+
+        public double Value
+        {
+            get => Get();
+            set => Set(value);
         }
     }
 }
