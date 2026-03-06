625a1a7463114ff6d1c999f5cc1b70d2f82a93e1
CO2 <majidabomehdi@gmail.com>
2026-03-06 13:17:30 +0200

    Add KeyboardHotkeySection property to ClientConfiguration

--- ClientCore/ClientConfiguration.cs
+++ ClientCore/ClientConfiguration.cs
@@ -365,6 +365,8 @@ namespace ClientCore
 
         public string KeyboardINI => clientDefinitionsIni.GetStringValue(SETTINGS, "KeyboardINI", "Keyboard.ini");
 
+        public string KeyboardHotkeySection => clientDefinitionsIni.GetStringValue(SETTINGS, "KeyboardHotkeySection", "Hotkey");
+
         public int MinimumIngameWidth => clientDefinitionsIni.GetIntValue(SETTINGS, "MinimumIngameWidth", 640);
 
         public int MinimumIngameHeight => clientDefinitionsIni.GetIntValue(SETTINGS, "MinimumIngameHeight", 480);
