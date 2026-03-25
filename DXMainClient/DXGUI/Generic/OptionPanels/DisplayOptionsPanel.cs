26fb60d8b458725d6335e6f017c68769ab8054e8
SadPencil <sadpencil@outlook.com>
2026-03-25 18:15:32 +0800

    Cache translation selection into local variable

--- DXMainClient/DXGUI/Generic/OptionPanels/DisplayOptionsPanel.cs
+++ DXMainClient/DXGUI/Generic/OptionPanels/DisplayOptionsPanel.cs
@@ -664,10 +664,9 @@ namespace DTAClient.DXGUI.Generic.OptionPanels
 
             IniSettings.ClientTheme.Value = (string)ddClientTheme.SelectedItem.Tag;
 
-            restartRequired = restartRequired || !IniSettings.Translation.ToString().Equals((string)ddTranslation.SelectedItem.Tag, StringComparison.InvariantCultureIgnoreCase);
-
-            IniSettings.Translation.Value = (string)ddTranslation.SelectedItem.Tag;
-
+            string newTranslationValue = (string)ddTranslation.SelectedItem.Tag;
+            restartRequired = restartRequired || !IniSettings.Translation.ToString().Equals(newTranslationValue, StringComparison.InvariantCultureIgnoreCase);
+            IniSettings.Translation.Value = newTranslationValue;
             Translation.ApplyTranslationGameFiles(IniSettings.Translation.Value);
 
             if (ClientConfiguration.Instance.ClientGameType == ClientType.TS)
