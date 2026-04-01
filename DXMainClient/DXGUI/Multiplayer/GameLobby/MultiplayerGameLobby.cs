e0d5da5fda09bac636efa921a628272679263550
CO2-code <majidabomehdi@gmail.com>
2026-04-01 23:30:57 +0300

    Gray out color text too

--- DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/MultiplayerGameLobby.cs
@@ -1258,6 +1258,7 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
                     bool taken = occupied.Contains(colorIndex) && colorIndex != currentColor;
 
                     dd.SetItemColorEnabled(j, !taken);
+                    dd.Items[j].TextColor = taken ? Color.Gray : Color.White;
                 }
             }
         }
