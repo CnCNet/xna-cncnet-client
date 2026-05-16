e34320297e4e8edb4a3972a57c25c5eab8b0814e
SadPencil <sadpencil@outlook.com>
2026-05-16 20:22:38 +0800

    Update SavedGame.cs

--- DXMainClient/Domain/SavedGame.cs
+++ DXMainClient/Domain/SavedGame.cs
@@ -42,7 +42,7 @@ namespace DTAClient.Domain
                     using (CfbStream scenarioDescStream = root.OpenStream("Scenario Description"))
                     {
                         if (scenarioDescStream.Length > MAX_SCENARIO_DESCRIPTION_BYTES)
-                            throw new InvalidDataException($"Scenario Description stream was unexpectedly large: {scenarioDescStream.Length} bytes. File: {FileName}");
+                            throw new InvalidDataException($"Scenario Description stream was unexpectedly large: {scenarioDescStream.Length} bytes.");
 
                         int scenarioDescLength = checked((int)scenarioDescStream.Length);
                         byte[] scenarioDescData = new byte[scenarioDescLength];
