b88594cabf721231c283b688868a0952df8f90fb
SadPencil <sadpencil@outlook.com>
2026-05-18 14:09:38 +0800

    rename

--- DXMainClient/Domain/DiscordHandler.cs
+++ DXMainClient/Domain/DiscordHandler.cs
@@ -163,13 +163,13 @@ namespace DTAClient.Domain
                 stateString += "🔒";
             CurrentPresence = new RichPresence()
             {
-                State = TrimDiscordPresenceText(stateString),
-                Details = TrimDiscordPresenceText($"{type} • {map} • {mode}"),
+                State = TruncateDiscordPresenceText(stateString),
+                Details = TruncateDiscordPresenceText($"{type} • {map} • {mode}"),
                 Assets = new Assets()
                 {
                     LargeImageKey = "logo",
                     SmallImageKey = sideKey,
-                    SmallImageText = TrimDiscordPresenceText(side)
+                    SmallImageText = TruncateDiscordPresenceText(side)
                 },
                 Timestamps = (client?.CurrentPresence.HasTimestamps() ?? false) && !resetTimer ?
                     client.CurrentPresence.Timestamps : Timestamps.Now
@@ -189,8 +189,8 @@ namespace DTAClient.Domain
                 stateString += "👑";
             CurrentPresence = new RichPresence()
             {
-                State = TrimDiscordPresenceText(stateString),
-                Details = TrimDiscordPresenceText($"{type} • {map} • {mode}"),
+                State = TruncateDiscordPresenceText(stateString),
+                Details = TruncateDiscordPresenceText($"{type} • {map} • {mode}"),
                 Assets = new Assets()
                 {
                     LargeImageKey = "logo"
@@ -208,13 +208,13 @@ namespace DTAClient.Domain
             string sideKey = new Regex("[^a-zA-Z0-9]").Replace(side.ToLower(), "");
             CurrentPresence = new RichPresence()
             {
-                State = TrimDiscordPresenceText(state),
-                Details = TrimDiscordPresenceText($"Skirmish • {map} • {mode}"),
+                State = TruncateDiscordPresenceText(state),
+                Details = TruncateDiscordPresenceText($"Skirmish • {map} • {mode}"),
                 Assets = new Assets()
                 {
                     LargeImageKey = "logo",
                     SmallImageKey = sideKey,
-                    SmallImageText = TrimDiscordPresenceText(side)
+                    SmallImageText = TruncateDiscordPresenceText(side)
                 },
                 Timestamps = (client?.CurrentPresence.HasTimestamps() ?? false) && !resetTimer ?
                     client.CurrentPresence.Timestamps : Timestamps.Now
@@ -230,12 +230,12 @@ namespace DTAClient.Domain
             CurrentPresence = new RichPresence()
             {
                 State = "Playing Mission",
-                Details = TrimDiscordPresenceText($"{mission} • {difficulty}"),
+                Details = TruncateDiscordPresenceText($"{mission} • {difficulty}"),
                 Assets = new Assets()
                 {
                     LargeImageKey = "logo",
                     SmallImageKey = sideKey,
-                    SmallImageText = TrimDiscordPresenceText(side)
+                    SmallImageText = TruncateDiscordPresenceText(side)
                 },
                 Timestamps = (client?.CurrentPresence.HasTimestamps() ?? false) && !resetTimer ?
                     client.CurrentPresence.Timestamps : Timestamps.Now
@@ -250,7 +250,7 @@ namespace DTAClient.Domain
             CurrentPresence = new RichPresence()
             {
                 State = "Playing Saved Game",
-                Details = TrimDiscordPresenceText(save),
+                Details = TruncateDiscordPresenceText(save),
                 Assets = new Assets()
                 {
                     LargeImageKey = "logo"
@@ -260,7 +260,7 @@ namespace DTAClient.Domain
             };
         }
 
-        private static string TrimDiscordPresenceText(string value)
+        private static string TruncateDiscordPresenceText(string value)
         {
             if (string.IsNullOrEmpty(value))
                 return value;
