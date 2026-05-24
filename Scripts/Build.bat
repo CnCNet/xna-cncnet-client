47890da293c1143fdc37a0fefb2b09d72aa43058
frg2089 <frg2089@outlook.com>
2026-05-24 08:01:09 +0000

    Update Scripts/Build.bat
    Co-authored-by: SadPencil <11227602+SadPencil@users.noreply.github.com>

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -1,8 +1,25 @@
 @echo off
-where pwsh > nul 2> nul
-if %errorlevel% equ 0 (
-  pwsh -ExecutionPolicy Bypass -File build.ps1
-) else (
-  echo "Please install PowerShell 7."
+REM Author: frg2089
+
+setlocal enabledelayedexpansion
+
+REM Check for pwsh.
+for %%e in (pwsh) do (
+  for /f "delims=" %%i in ('where %%e 2^>nul') do (
+    set "ps_path=%%i"
+    goto :found
+  )
 )
-pause
+
+REM No PowerShell executable found.
+echo Error: Unable to find command pwsh. Please make sure PowerShell 7 is installed. >&2
+exit /b 1
+
+:found
+echo Found PowerShell at: %ps_path%
+
+REM Pass the value outside of setlocal scope.
+endlocal & set "ps_path=%ps_path%"
+
+REM Run the same-name .ps1 script in this directory and forward all arguments.
+"%ps_path%" -ExecutionPolicy Bypass -Command "& '%~dp0%~n0.ps1' %*"
