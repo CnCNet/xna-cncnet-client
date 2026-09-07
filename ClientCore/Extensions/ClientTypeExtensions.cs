026087306a4c1d39aa7edbd03525eac032773b43
SadPencil <sadpencil@outlook.com>
2026-09-07 23:25:36 +0800

    Format ClientTypeExtensions.cs

--- ClientCore/Extensions/ClientTypeExtensions.cs
+++ ClientCore/Extensions/ClientTypeExtensions.cs
@@ -3,44 +3,43 @@ using System;
 
 using ClientCore.Enums;
 
-namespace ClientCore.Extensions
+namespace ClientCore.Extensions;
+
+public static class ClientTypeExtensions
 {
-    public static class ClientTypeExtensions
+    private static readonly string unknownClientTypeErrorMsg = string.Format((
+            "It seems the client configuration was not migrated to accommodate for the v2.12 changes. " +
+            "Please specify 'ClientGameType' in '[Settings]' section of the 'ClientDefinitions.ini' file " +
+            "(allowed options: {0}).\n\n" +
+            "Please refer to documentation of the client {1} for more details. This link can also be found in the log file.").L10N("Client:Main:ClientGameTypeNotFoundException"),
+            EnumExtensions.GetNames<ClientType>(),
+            "https://github.com/CnCNet/xna-cncnet-client/");
+
+    extension(ClientType)
     {
-        private static readonly string unknownClientTypeErrorMsg = string.Format((
-                "It seems the client configuration was not migrated to accommodate for the v2.12 changes. " +
-                "Please specify 'ClientGameType' in '[Settings]' section of the 'ClientDefinitions.ini' file " +
-                "(allowed options: {0}).\n\n" +
-                "Please refer to documentation of the client {1} for more details. This link can also be found in the log file.").L10N("Client:Main:ClientGameTypeNotFoundException"),
-                EnumExtensions.GetNames<ClientType>(),
-                "https://github.com/CnCNet/xna-cncnet-client/");
-
-        extension(ClientType)
+        public static ClientType FromString(string value) => value switch
         {
-            public static ClientType FromString(string value) => value switch
-            {
-                "TD" => ClientType.TD,
-                "RA" => ClientType.RA,
-                "TS" => ClientType.TS,
-                "YR" => ClientType.YR,
-                "Ares" => ClientType.Ares,
-                _ => throw new Exception(unknownClientTypeErrorMsg),
-            };
-        }
+            "TD" => ClientType.TD,
+            "RA" => ClientType.RA,
+            "TS" => ClientType.TS,
+            "YR" => ClientType.YR,
+            "Ares" => ClientType.Ares,
+            _ => throw new Exception(unknownClientTypeErrorMsg),
+        };
+    }
 
-        extension(ClientType clientType)
+    extension(ClientType clientType)
+    {
+        public uint? ToSteamAppId() => clientType switch
         {
-            public uint? ToSteamAppId() => clientType switch
-            {
-                ClientType.TD => 2229830,
-                ClientType.RA => 2229840,
-                ClientType.TS => 2229880,
-                ClientType.YR or ClientType.Ares => 2229850,
-                // You may return null if a new ClientType does not have a corresponding Steam App ID
-                // Throw on unexpected ClientType
-                _ => throw new Exception(unknownClientTypeErrorMsg),
-            };
-        }
+            ClientType.TD => 2229830,
+            ClientType.RA => 2229840,
+            ClientType.TS => 2229880,
+            ClientType.YR or ClientType.Ares => 2229850,
+            // You may return null if a new ClientType does not have a corresponding Steam App ID
+            // Throw on unexpected ClientType
+            _ => throw new Exception(unknownClientTypeErrorMsg),
+        };
     }
 }
 
