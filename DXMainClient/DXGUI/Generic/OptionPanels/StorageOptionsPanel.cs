941fc07e9722c885341019116275d42cd701f12c
[11EJ11] <jacobseliot54@gmail.com>
2026-09-11 08:09:50 +1200

    Rename ReplayGame to YRReplayGame Rename ReplayManager path helpers to public GetReplay*

--- DXMainClient/DXGUI/Generic/OptionPanels/StorageOptionsPanel.cs
+++ DXMainClient/DXGUI/Generic/OptionPanels/StorageOptionsPanel.cs
@@ -413,7 +413,7 @@ class StorageOptionsPanel : XNAOptionsPanel
 
         try
         {
-            DirectoryInfo directory = ReplayManager.GetDirectory();
+            DirectoryInfo directory = ReplayManager.GetReplayDirectory();
             if (directory.Exists)
             {
                 foreach (FileInfo file in directory.EnumerateFiles(ReplayManager.SearchPattern))
