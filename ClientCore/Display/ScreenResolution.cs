dbc82957f4d6716ba8ea1059f1b5cf4886872756
SadPencil <sadpencil@outlook.com>
2026-09-12 11:55:06 +0800

    Format file

--- ClientCore/Display/ScreenResolution.cs
+++ ClientCore/Display/ScreenResolution.cs
@@ -4,66 +4,65 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-namespace ClientCore.Display
+namespace ClientCore.Display;
+
+/// <summary>
+/// A single screen resolution.
+/// </summary>
+public sealed record ScreenResolution : IComparable<ScreenResolution>
 {
+
     /// <summary>
-    /// A single screen resolution.
+    /// The width of the resolution in pixels.
     /// </summary>
-    public sealed record ScreenResolution : IComparable<ScreenResolution>
+    public int Width { get; }
+
+    /// <summary>
+    /// The height of the resolution in pixels.
+    /// </summary>
+    public int Height { get; }
+
+    public ScreenResolution(int width, int height)
     {
+        Width = width;
+        Height = height;
+    }
 
-        /// <summary>
-        /// The width of the resolution in pixels.
-        /// </summary>
-        public int Width { get; }
+    public ScreenResolution(Rectangle rectangle)
+    {
+        Width = rectangle.Width;
+        Height = rectangle.Height;
+    }
 
-        /// <summary>
-        /// The height of the resolution in pixels.
-        /// </summary>
-        public int Height { get; }
+    public ScreenResolution(string resolution)
+    {
+        List<int> resolutionList = resolution.Trim().Split('x').Take(2).Select(int.Parse).ToList();
+        Width = resolutionList[0];
+        Height = resolutionList[1];
+    }
 
-        public ScreenResolution(int width, int height)
-        {
-            Width = width;
-            Height = height;
-        }
+    public static implicit operator ScreenResolution(string resolution) => new(resolution);
 
-        public ScreenResolution(Rectangle rectangle)
-        {
-            Width = rectangle.Width;
-            Height = rectangle.Height;
-        }
+    public sealed override string ToString() => Width + "x" + Height;
 
-        public ScreenResolution(string resolution)
-        {
-            List<int> resolutionList = resolution.Trim().Split('x').Take(2).Select(int.Parse).ToList();
-            Width = resolutionList[0];
-            Height = resolutionList[1];
-        }
+    public static implicit operator string(ScreenResolution resolution) => resolution.ToString();
 
-        public static implicit operator ScreenResolution(string resolution) => new(resolution);
+    public void Deconstruct(out int width, out int height)
+    {
+        width = this.Width;
+        height = this.Height;
+    }
 
-        public sealed override string ToString() => Width + "x" + Height;
+    public static implicit operator ScreenResolution((int Width, int Height) resolutionTuple) => new(resolutionTuple.Width, resolutionTuple.Height);
 
-        public static implicit operator string(ScreenResolution resolution) => resolution.ToString();
+    public static implicit operator (int Width, int Height)(ScreenResolution resolution) => new(resolution.Width, resolution.Height);
 
-        public void Deconstruct(out int width, out int height)
-        {
-            width = this.Width;
-            height = this.Height;
-        }
+    public bool Fits(ScreenResolution child) => this.Width >= child.Width && this.Height >= child.Height;
 
-        public static implicit operator ScreenResolution((int Width, int Height) resolutionTuple) => new(resolutionTuple.Width, resolutionTuple.Height);
-
-        public static implicit operator (int Width, int Height)(ScreenResolution resolution) => new(resolution.Width, resolution.Height);
-
-        public bool Fits(ScreenResolution child) => this.Width >= child.Width && this.Height >= child.Height;
-
-        public int CompareTo(ScreenResolution? other)
-        {
-            if (other is null)
-                return 1;
-            return (this.Width, this.Height).CompareTo((other.Width, other.Height));
-        }
+    public int CompareTo(ScreenResolution? other)
+    {
+        if (other is null)
+            return 1;
+        return (this.Width, this.Height).CompareTo((other.Width, other.Height));
     }
 }
