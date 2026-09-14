355ea4fbe86f9a84df909e9a736af7f80fd47fa2
[11EJ11] <jacobseliot54@gmail.com>
2026-09-14 16:02:07 +1200

    Fix indentation

--- DXMainClient/DXGUI/Campaign/CampaignSelector.cs
+++ DXMainClient/DXGUI/Campaign/CampaignSelector.cs
@@ -476,7 +476,7 @@ namespace DTAClient.DXGUI.Campaign
                 case ClientType.TS:
                     spawnIniSettings.AddKey("Firestorm", mission.RequiredAddon.ToString(CultureInfo.InvariantCulture));
                     break;
-                    // TODO figure out the RA one
+                // TODO figure out the RA one
             }
 
             spawnIniSettings.AddKey("CustomLoadScreen", LoadingScreenController.GetLoadScreenName(mission.Side.ToString()));
