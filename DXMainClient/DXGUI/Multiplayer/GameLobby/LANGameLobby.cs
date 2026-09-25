840cb6c394cfbd31430492ce1734b1b24f8ddb16
SadPencil <sadpencil@outlook.com>
2026-09-25 11:02:32 +0800

    Post update for "Fix crash when joining LAN Lobby filled with AI players"

--- DXMainClient/DXGUI/Multiplayer/GameLobby/LANGameLobby.cs
+++ DXMainClient/DXGUI/Multiplayer/GameLobby/LANGameLobby.cs
@@ -315,7 +315,7 @@ namespace DTAClient.DXGUI.Multiplayer.GameLobby
 
             Players.Add(lpInfo);
 
-            if (Players.Count + AIPlayers.Count > MAX_PLAYER_COUNT && AIPlayers.Count > 0)
+            while (Players.Count + AIPlayers.Count > MAX_PLAYER_COUNT && AIPlayers.Count > 0)
                 AIPlayers.RemoveAt(AIPlayers.Count - 1);
 
             if (IsHost && Players.Count == 1)
